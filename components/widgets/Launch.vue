39c21ca48f4d37665f7d6e6d69377c182d27a916
Marko Banušić <mbanusic@gmail.com>
2020-12-08 15:20:40 +0100

    fix link for launch banner

--- components/widgets/Launch.vue
+++ components/widgets/Launch.vue
@@ -1,6 +1,6 @@
 <template>
   <iframe
-    src="https://apps.adriaticmedia.hr/production/project_codename/rc3/megabillboard.html"
+    src="https://www.telegram.hr/wp-content/themes/telegram2-desktop/templates/native/erste/tgneue/megabillboard.html"
     style="width: 100%; height: 500px"
   ></iframe>
 </template>
