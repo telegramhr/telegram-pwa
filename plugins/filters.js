c7b6e830a3cbf6a645e25f8935dd5105fc2d7396
Marko Banušić <mbanusic@gmail.com>
2020-12-30 15:26:29 +0100

    new time display

--- plugins/filters.js
+++ plugins/filters.js
@@ -3,8 +3,29 @@ import { parse } from 'node-html-parser'
 
 export default () => {
   Vue.filter('parseTime', function (value) {
-    const date = new Date(value * 1000)
-    return new Intl.DateTimeFormat('hr').format(date)
+    const now = Date.now()
+    value = value * 1000
+    if (value < now - 12 * 3600 * 1000) {
+      const date = new Date(value)
+      return new Intl.DateTimeFormat('hr').format(date)
+    } else {
+      const diff = (now - value) / 1000
+      let h = diff / 3600
+      if (h > 1) {
+        h = Math.round(h)
+        console.log(h)
+        if (h < 5) {
+          return 'prije ' + h + ' sata'
+        }
+        return 'prije ' + h + ' sati'
+      }
+      let m = diff % 3600
+      m = Math.round(m / 60)
+      if (m % 10 < 5) {
+        return 'prije ' + m + ' minute'
+      }
+      return 'prije ' + m + ' minuta'
+    }
   })
   Vue.filter('parseCat', function (value) {
     if (!value || value === 'null') {
