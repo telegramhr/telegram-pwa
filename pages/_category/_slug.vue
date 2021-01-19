5a623f155ff08c14bf65335466870715c4c4cec0
Marko Banušić <mbanusic@gmail.com>
2021-01-19 12:43:27 +0100

    OG tags fix

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -571,21 +571,34 @@ export default {
           name: 'og:description',
           content: this.post.social.description,
         },
-        { hid: 'og:type', name: 'og:type', content: 'article' },
-        { hid: 'og:title', name: 'og:title', content: this.post.social.title },
+        {
+          hid: 'og:type',
+          name: 'og:type',
+          property: 'og:type',
+          content: 'article',
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          property: 'og:title',
+          content: this.post.social.title,
+        },
         {
           hid: 'og:image',
           name: 'og:image',
+          property: 'og:image',
           content: this.post.social.image,
         },
         {
           hid: 'og:url',
           name: 'og:url',
+          property: 'og:url',
           content: this.post.social.path,
         },
         {
           hid: 'og:app_id',
           name: 'og:app_id',
+          property: 'og:app_id',
           content: '1383786971938581',
         },
         {
