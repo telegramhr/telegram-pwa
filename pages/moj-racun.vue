6a98e55033efb26ec21ae9a78bd54b2223013090
Marko Banušić <mbanusic@gmail.com>
2021-02-08 16:11:49 +0100

    persist user, move to component

--- pages/moj-racun.vue
+++ pages/moj-racun.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="my-account"></div>
+  <div class="main-container flex category-page">
+    <div class="full flex relative single-article">
+      <client-only>
+        <theader></theader>
+      </client-only>
+    </div>
+    <div class="container full relative">
+      <client-only>
+        <piano-account></piano-account>
+      </client-only>
+    </div>
+    <tfooter></tfooter>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'MojRacun',
-  mounted() {
-    this.$nextTick(() => {
-      const tp = window.tp || []
-      tp.push([
-        'init',
-        function () {
-          tp.myaccount.show({
-            displayMode: 'inline',
-            containerSelector: '#my-account',
-          })
-        },
-      ])
-    })
-  },
 }
 </script>
