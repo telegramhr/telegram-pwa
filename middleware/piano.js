b2f57d0817c71280cd5bef2707b93e5b69eea7d0
Marko Banušić <mbanusic@gmail.com>
2021-02-17 11:05:12 +0100

    remove cxsense

--- middleware/piano.js
+++ middleware/piano.js
@@ -2,9 +2,5 @@
 export default function ({ route }) {
   if (process.client) {
     window.tp.experience.execute()
-
-    window.cX = window.cX || {}; window.cX.callQueue = window.cX.callQueue || [];
-    window.cX.callQueue.push(['setSiteId', '1139723852373953244']);
-    window.cX.callQueue.push(['sendPageViewEvent']);
   }
 }
