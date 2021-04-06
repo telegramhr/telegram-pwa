fa19c4ed6dcc3650367311a1d952750ff915c50d
Marko Banušić <mbanusic@gmail.com>
2021-04-06 12:56:05 +0200

    fix latest

--- components/Elements/Latest.vue
+++ components/Elements/Latest.vue
@@ -2,7 +2,7 @@
   <div class="full flex desktop-only">
     <h2 class="full flex section-title">Najnovije</h2>
     <mini
-      v-for="post in $store.state.latest.posts"
+      v-for="post in $store.state.latest.posts[1]"
       :key="post.id"
       :post="post"
     ></mini>
