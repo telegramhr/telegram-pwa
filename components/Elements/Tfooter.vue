a91621f542436b1f812ccfd690c694b877f378b4
Marijan Mikolić <marijan.mikolic@gmail.com>
2021-04-13 10:49:39 +0200

    Pretplata pre-footer widget (and ticker btn animation)

--- components/Elements/Tfooter.vue
+++ components/Elements/Tfooter.vue
@@ -1,75 +1,102 @@
 <template>
-  <footer class="full flex mobile-side-pad">
-    <div class="container">
-      <div class="full flex center">
-        <div class="half main-footer-logo center">
-          <img src="@/assets/img/telegram_logo_white.svg" alt="Telegram logo" />
+  <div class="full flex relative">
+    <div
+      class="full pretplata-ticker pretplata-prefooter relative"
+      v-show="!$store.state.user.token"
+    >
+      <div class="container flex relative">
+        <h2 class="full center-text">
+          Bespoštedno novinarstvo koje gura društvo naprijed.
+        </h2>
+        <p class="full center-text">
+          Za neograničeno čitanje Telegrama i podršku istraživačkim serijalima,
+          odaberite jedan od paketa.
+        </p>
+        <div class="full center">
+          <app-link to="/pretplata" class="btn animate"
+            >Pretplatite se</app-link
+          >
+          <div class="btn animate" @click="login">Već imam pretplatu</div>
         </div>
       </div>
-      <div class="full flex center">
-        <div class="full logo-line flex">
-          <img src="@/assets/img/super1_logo_white.svg" alt="Telegram logo" />
-          <img
-            src="@/assets/img/telesport_logo_white.svg"
-            alt="Telegram logo"
-          />
-          <img src="@/assets/img/tg_studio_white.svg" alt="Telegram logo" />
+    </div>
+    <footer class="full flex mobile-side-pad">
+      <div class="container">
+        <div class="full flex center">
+          <div class="half main-footer-logo center">
+            <img
+              src="@/assets/img/telegram_logo_white.svg"
+              alt="Telegram logo"
+            />
+          </div>
+        </div>
+        <div class="full flex center">
+          <div class="full logo-line flex">
+            <img src="@/assets/img/super1_logo_white.svg" alt="Telegram logo" />
+            <img
+              src="@/assets/img/telesport_logo_white.svg"
+              alt="Telegram logo"
+            />
+            <img src="@/assets/img/tg_studio_white.svg" alt="Telegram logo" />
+          </div>
+        </div>
+        <div class="full flex social-links center">
+          <app-link
+            to="https://www.facebook.com/www.telegram.hr/"
+            class="center animate"
+            aria-label="Facebook"
+            ><i class="fab fa-facebook-f"></i
+          ></app-link>
+          <app-link
+            to="https://twitter.com/telegramhr/"
+            class="center animate"
+            aria-label="Twitter"
+            ><i class="fab fa-twitter"></i
+          ></app-link>
+          <app-link
+            to="https://www.instagram.com/telegram.hr/"
+            class="center animate"
+            aria-label="Instagram"
+            ><i class="fab fa-instagram"></i
+          ></app-link>
+          <app-link
+            to="https://www.telegram.hr/feed"
+            class="center animate"
+            aria-label="RSS"
+            ><i class="fas fa-rss"></i
+          ></app-link>
+        </div>
+        <div class="full flex services center">
+          <app-link to="/pretplata" class="animate">Pretplata</app-link>
+          <app-link to="/stranica/impressum" class="animate"
+            >Impressum</app-link
+          >
+          <app-link to="/stranica/oglasavanje" class="animate"
+            >Oglašavanje</app-link
+          >
+          <app-link to="/stranica/kontaktirajte-nas" class="animate"
+            >Kontakt</app-link
+          >
+          <app-link to="https://www.telegramgrupa.hr/jobs.php" class="animate"
+            >Karijere</app-link
+          >
+          <app-link to="/stranica/uvjeti-koristenja" class="animate"
+            >Uvjeti korištenja</app-link
+          >
+          <app-link to="/stranica/pravila-privatnosti" class="animate"
+            >Pravila o privatnosti</app-link
+          >
+        </div>
+        <div class="copyright full center-text">
+          Sva prava pridržana &copy; {{ new Date().getFullYear() }} Telegram
+          Media Grupa d.o.o.
         </div>
       </div>
-      <div class="full flex social-links center">
-        <app-link
-          to="https://www.facebook.com/www.telegram.hr/"
-          class="center animate"
-          aria-label="Facebook"
-          ><i class="fab fa-facebook-f"></i
-        ></app-link>
-        <app-link
-          to="https://twitter.com/telegramhr/"
-          class="center animate"
-          aria-label="Twitter"
-          ><i class="fab fa-twitter"></i
-        ></app-link>
-        <app-link
-          to="https://www.instagram.com/telegram.hr/"
-          class="center animate"
-          aria-label="Instagram"
-          ><i class="fab fa-instagram"></i
-        ></app-link>
-        <app-link
-          to="https://www.telegram.hr/feed"
-          class="center animate"
-          aria-label="RSS"
-          ><i class="fas fa-rss"></i
-        ></app-link>
+      <div class="full center">
+        <ad-unit id="telegram_sticky"></ad-unit>
       </div>
-      <div class="full flex services center">
-        <app-link to="/pretplata" class="animate">Pretplata</app-link>
-        <app-link to="/stranica/impressum" class="animate">Impressum</app-link>
-        <app-link to="/stranica/oglasavanje" class="animate"
-          >Oglašavanje</app-link
-        >
-        <app-link to="/stranica/kontaktirajte-nas" class="animate"
-          >Kontakt</app-link
-        >
-        <app-link to="https://www.telegramgrupa.hr/jobs.php" class="animate"
-          >Karijere</app-link
-        >
-        <app-link to="/stranica/uvjeti-koristenja" class="animate"
-          >Uvjeti korištenja</app-link
-        >
-        <app-link to="/stranica/pravila-privatnosti" class="animate"
-          >Pravila o privatnosti</app-link
-        >
-      </div>
-      <div class="copyright full center-text">
-        Sva prava pridržana &copy; {{ new Date().getFullYear() }} Telegram Media
-        Grupa d.o.o.
-      </div>
-    </div>
-    <div class="full center">
-      <ad-unit id="telegram_sticky"></ad-unit>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
 
 <script>
