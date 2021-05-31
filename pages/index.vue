4084634a3f80f62277b1dacb4d51f5c3e7665715
Marko Banušić <mbanusic@gmail.com>
2021-05-31 18:12:59 +0200

    remove elections widget

--- pages/index.vue
+++ pages/index.vue
@@ -30,16 +30,13 @@
         <section
           class="three-fourths mobile-side-pad flex-responsive flex relative the-big-gs stretch elevate-over-section"
         >
-          <client-only>
-            <izbori></izbori>
-          </client-only>
           <div
-            class="five-sixths column-horizontal-pad flex-responsive flex column-bottom-border column-right-border column-top-pad column-top-border"
+            class="five-sixths column-horizontal-pad flex-responsive flex column-bottom-border column-right-border"
           >
             <featured :key="posts[0].id" :post="posts[0]"></featured>
           </div>
           <aside
-            class="sixth desktop-only flex column-horizontal-pad column-bottom-border column-top-border"
+            class="sixth desktop-only flex column-horizontal-pad column-bottom-border"
           >
             <client-only>
               <weather></weather>
