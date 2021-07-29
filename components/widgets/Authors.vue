d30246d903c118e9d26516ccb723f8cc85fab038
Marijan Mikolić <marijan.mikolic@gmail.com>
2021-07-29 08:57:44 +0200

    Fancy authors widget

---
+++ components/widgets/Authors.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container flex relative stretch">
+    <div class="full column-full-pad mobile-side-pad">
+      <h2 class="full section-title">Autori</h2>
+    </div>
+    <div class="full flex fancy-authors-widget stretch">
+      <div class="fourth flex column-horizontal-pad mobile-side-pad stretch">
+        <a class="full flex fancy-author-tile">
+          <img
+            src="https://www.telegram.hr/wp-content/uploads/2019/09/tg_krsul.png"
+            alt="Dora Kršul"
+          />
+          <div class="full flex fancy-author-content">
+            <h2 class="fancy-author-name">Dora Kršul</h2>
+            <h3 class="fancy-author-title faded">Stručnjakinja za školstvo</h3>
+            <p class="fancy-article-preview">
+              Prije tri tjedna objavili smo snimku zagrebačkog prodekana kako
+              krši zakon. Kako je moguće da on još nije smijenjen?
+            </p>
+            <h5 class="article-meta">
+              <span class="meta-date">Prije 45 minuta</span>
+              <span class="meta-preporuke">21 preporuka</span>
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div class="fourth flex column-horizontal-pad mobile-side-pad stretch">
+        <a class="full flex fancy-author-tile">
+          <img
+            src="https://www.telegram.hr/wp-content/uploads/2018/07/tg_klaric.png"
+            alt="Jasmin Klarić"
+          />
+          <div class="full flex fancy-author-content">
+            <h2 class="fancy-author-name">Jasmin Klarić</h2>
+            <h3 class="fancy-author-title faded">
+              Politički analitičar i urednik
+            </h3>
+            <p class="fancy-article-preview">
+              Ministarstvo zdravstva opet skriva informacije: koliko smo platili
+              još jednu propalu aplikaciju za covid?
+            </p>
+            <h5 class="article-meta">
+              <span class="meta-date">Prije 45 minuta</span>
+              <span class="meta-preporuke">21 preporuka</span>
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div class="fourth flex column-horizontal-pad mobile-side-pad stretch">
+        <a class="full flex fancy-author-tile">
+          <img
+            src="https://www.telegram.hr/wp-content/uploads/2019/06/tg-sanja-modric.png"
+            alt="Sanja Modrić"
+          />
+          <div class="full flex fancy-author-content">
+            <h2 class="fancy-author-name">Sanja Modrić</h2>
+            <h3 class="fancy-author-title faded">
+              Analitičarka društvenih pojava
+            </h3>
+            <p class="fancy-article-preview">
+              Opći darmar u Škorinoj stranci bio je samo pitanje vremena. Što se
+              uopće moglo očekivati od takve nakupine kadrova?
+            </p>
+            <h5 class="article-meta">
+              <span class="meta-date">Prije 45 minuta</span>
+              <span class="meta-preporuke">21 preporuka</span>
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div class="fourth flex column-horizontal-pad mobile-side-pad stretch">
+        <a class="full flex fancy-author-tile">
+          <img
+            src="https://www.telegram.hr/wp-content/uploads/2020/04/repecki2.png"
+            alt="Marko Repecki"
+          />
+          <div class="full flex fancy-author-content">
+            <h2 class="fancy-author-name">Marko Repecki</h2>
+            <h3 class="fancy-author-title faded">
+              Komentator ekonomije i biznisa
+            </h3>
+            <p class="fancy-article-preview">
+              Hrvatska je nakon korone opet prezadužena zemlja. Bez reformi,
+              koje uporno odbija, Vlada će biti prisiljena dizati poreze
+            </p>
+            <h5 class="article-meta">
+              <span class="meta-date">Prije 45 minuta</span>
+              <span class="meta-preporuke">21 preporuka</span>
+            </h5>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Authors',
+}
+</script>
