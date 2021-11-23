7bd4da00803cc83350d2f079844e71e4bcbf732b
Marijan Mikolić <marijan.mikolic@gmail.com>
2021-11-23 13:38:37 +0100

    PZ special style and text changes 3

--- pages/pitanje-zdravlja.vue
+++ pages/pitanje-zdravlja.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-container flex pitanje-zdravlja">
+  <div class="main-container flex pitanje-zdravlja pz-homepage">
     <div class="full pz-header">
       <app-link to="/" class="full center pz-subheader relative"
         ><img src="@/assets/img/telegram_logo_white.svg" alt="Telegram logo"
@@ -36,7 +36,7 @@
         </nav>
       </div>
     </div>
-    <div class="full pz-hero center">
+    <div class="full pz-hero center relative">
       <div class="container column-full-pad mobile-side-pad">
         <h1 class="full center-text">
           <span data-aos="fade-up" data-aos-delay="750">Pitanje</span
@@ -54,7 +54,7 @@
           data-aos="fade-up"
           data-aos-delay="2000"
         >
-          Platforma koja govori o zdravstvenim temama na kritički način.
+          Kritički progovaramo o zdravstvenim temama.
         </p>
         <div
           class="full center column-top-pad hide"
@@ -111,21 +111,7 @@
             data-aos="fade-up"
             data-aos-delay="1250"
           >
-            Platforma koja o zdravlju piše na kritički način.
-          </p>
-          <p
-            class="full pz-intro center-text"
-            data-aos="fade-up"
-            data-aos-delay="1500"
-          >
-            Objavljujemo točne informacije.
-          </p>
-          <p
-            class="full pz-intro center-text"
-            data-aos="fade-up"
-            data-aos-delay="1750"
-          >
-            Želimo pomoći u razvijanju zdravstvene pismenosti Hrvata.
+            Platforma koja pomaže u razvoju zdravstvene pismenosti građana.
           </p>
         </div>
         <div class="full center column-full-pad">
@@ -202,14 +188,6 @@
             data-aos="fade-up"
             data-aos-delay="1250"
           >
-            Pitanje zdravlja lansirano je s ciljem promocije zdravstvene
-            pismenosti kod Hrvata.
-          </p>
-          <p
-            class="full pz-intro center-text"
-            data-aos="fade-up"
-            data-aos-delay="1500"
-          >
             Napišite koja zdravstvena tema vas zanima, istražit ćemo je i
             ponuditi točne odgovore.
           </p>
@@ -237,12 +215,12 @@
         </h2>
         <div class="full center pz-logos">
           <img
-            src="https://www.telegram.hr/wp-content/uploads/2021/11/logo.png"
-            alt="Sve za nju logo"
-          />
-          <img
             src="https://www.telegram.hr/wp-content/uploads/2021/11/logo-kuz-120x96-1.jpg"
             alt="Savez udruga u zdravstvu logo"
+          />
+          <img
+            src="https://www.telegram.hr/wp-content/uploads/2021/11/logo.png"
+            alt="Sve za nju logo"
           />
         </div>
         <div class="full center">
@@ -263,8 +241,8 @@
 </template>
 
 <script>
-/* import AOS from 'aos'
-import 'aos/dist/aos.css' */
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 export default {
   name: 'PitanjeZdravlja',
@@ -297,319 +275,11 @@ export default {
       }
     },
   },
-  /* mounted() {
+  mounted() {
     AOS.init({
       duration: 1000,
       delay: 250,
     })
-  }, */
+  },
 }
 </script>
-
-<style>
-.pitanje-zdravlja {
-  background: linear-gradient(to top, #ededed, #fefefe);
-  background-attachment: fixed;
-  font-family: neuzeit-grotesk, sans-serif;
-  font-weight: 300;
-  color: #111;
-}
-.pitanje-zdravlja .header-billboard {
-  display: none;
-}
-.pz-header {
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  background: white;
-  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.15);
-  z-index: 10;
-}
-.pz-header .container {
-  padding-left: 148px;
-  position: relative;
-}
-.pz-header-badge {
-  width: 84px;
-  position: absolute;
-  top: -36px;
-  left: 24px;
-}
-.pz-header-logo {
-  position: absolute;
-  top: 45px;
-  left: 26px;
-  width: 37px;
-}
-.pz-header nav a {
-  padding: 16px;
-  font-size: 16px;
-  font-size: 0.8rem;
-  opacity: 0.7;
-}
-.pz-header nav a:hover {
-  opacity: 1;
-}
-.pz-hero {
-  height: 100vh;
-}
-.pitanje-zdravlja .related-articles {
-  display: none;
-}
-.pz-special-tile {
-  border: 1px solid #ededed;
-  background-color: white;
-  height: 150px;
-  border-radius: 20px;
-  margin-top: 24px;
-  padding: 24px;
-}
-.pz-special-tile h2 {
-  background: -webkit-linear-gradient(#27a9e0, #0a80c5);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  text-transform: uppercase;
-  font-size: 30px;
-  font-size: 1.5rem;
-  font-family: neuzeit-grotesk, sans-serif;
-  font-weight: 300;
-}
-.pz-hero h1 {
-  font-size: 90px;
-  font-size: 4.5rem;
-  text-transform: uppercase;
-  font-family: neuzeit-grotesk, sans-serif;
-  font-weight: 300;
-  margin-bottom: 16px;
-}
-.pz-hero h1 span:first-child {
-  letter-spacing: 0.18em;
-  font-weight: 700;
-}
-.pz-hero h1 span:nth-child(2) {
-  letter-spacing: 0.05em;
-  color: #29abe2;
-}
-.pz-intro {
-  font-size: 24px;
-  font-size: 1.2rem;
-  opacity: 0.7;
-  margin-top: 16px;
-}
-.pz-button {
-  background: linear-gradient(to bottom, #27a9e0, #0a80c5);
-  color: white;
-  font-size: 18px;
-  font-size: 0.9rem;
-  padding: 12px 48px;
-  text-transform: uppercase;
-  font-weight: 700;
-  letter-spacing: 2px;
-  border-radius: 25px;
-  cursor: pointer;
-  margin-top: 36px;
-}
-.pitanje-zdravlja .article h2 {
-  font-family: neuzeit-grotesk, sans-serif;
-  font-weight: 400;
-  letter-spacing: -0.02em;
-  font-size: 28px;
-  font-size: 1.4rem;
-  line-height: 1.1em;
-}
-.pitanje-zdravlja .block-2 .article-featured h2 {
-  font-size: 30px;
-  font-size: 1.5rem;
-}
-.pitanje-zdravlja .article-standard h2 {
-  font-size: 24px;
-  font-size: 1.2rem;
-}
-.pitanje-zdravlja .article-big h2 {
-  font-size: 40px;
-  font-size: 2rem;
-}
-.pitanje-zdravlja .article h3.overtitle,
-.pz-blue {
-  color: #0a80c5;
-}
-.pitanje-zdravlja .article img {
-  border-radius: 30px;
-}
-.pitanje-zdravlja .article-featured img {
-  margin-bottom: 16px;
-}
-.pitanje-zdravlja .article-big img {
-  margin-bottom: 8px;
-}
-.pz-dynamic {
-  font-weight: 300;
-}
-.pitanje-zdravlja .article .article-meta {
-  display: none;
-}
-.pz-fancy-slide h2,
-.pz-fancy-title {
-  font-size: 54px;
-  font-size: 2.7rem;
-  font-family: neuzeit-grotesk, sans-serif;
-  font-weight: 700;
-  margin-bottom: 32px;
-}
-.pz-fancy-slide::before {
-  content: ' ';
-  width: 100%;
-  height: 70%;
-  background-color: white;
-  position: absolute;
-  top: 15%;
-  left: 0;
-}
-.pz-fancy-line {
-  width: 1px;
-  background-color: #27a9e0;
-  height: 30vh;
-}
-.pz-fancy-slide textarea {
-  width: 90%;
-  max-width: 640px;
-  height: 100px;
-  margin-top: 48px;
-}
-.pitanje-zdravlja .arrow-indicator {
-  position: absolute;
-  top: 80vh;
-  width: 100%;
-  left: 0;
-  z-index: 10;
-}
-.pitanje-zdravlja .arrow-indicator div {
-  width: 25px;
-  height: 25px;
-  border-right: 5px solid #27a9e0;
-  border-bottom: 5px solid #27a9e0;
-  transform: rotate(45deg);
-  opacity: 0;
-  top: 0;
-  position: relative;
-  animation-name: point-down;
-  animation-duration: 4s;
-  animation-iteration-count: infinite;
-  animation-timing-function: ease-in-out;
-}
-.pz-widget-overtitle {
-  text-transform: uppercase;
-  color: #0a80c5;
-}
-.pitanje-zdravlja .native-signature .tg-studio {
-  width: 70px;
-  height: 90px;
-  object-fit: contain;
-}
-.pitanje-zdravlja .native-signature p {
-  max-width: 660px;
-  line-height: 1.2em;
-  opacity: 0.7;
-  margin: 0 auto;
-}
-.pz-logos img {
-  width: 120px;
-  height: 80px;
-  margin: 16px;
-  object-fit: contain;
-  object-position: center center;
-  mix-blend-mode: darken;
-}
-.pitanje-zdravlja header nav,
-header .subheader {
-  display: none;
-}
-.pz-subheader {
-  z-index: 11;
-  padding: 7px 0;
-  background-color: #212121;
-}
-.pz-subheader img {
-  height: 15px;
-}
-@keyframes point-down {
-  0% {
-    opacity: 0;
-    top: 0;
-  }
-  40% {
-    opacity: 1;
-    top: 30px;
-  }
-  60% {
-    opacity: 1;
-    top: 30px;
-  }
-  100% {
-    opacity: 0;
-    top: 60px;
-  }
-}
-@media screen and (max-width: 1180px) {
-  .pz-header nav {
-    flex-wrap: nowrap;
-    overflow-x: scroll;
-    height: 62px;
-  }
-  .pz-header nav a {
-    padding: 12px;
-  }
-  .pz-hero h1 {
-    font-size: 18vw;
-  }
-  .pz-fancy-slide h2,
-  .pz-fancy-title {
-    font-size: 40px;
-    font-size: 2rem;
-    margin-bottom: 24px;
-  }
-  .pz-fancy-line {
-    margin-top: 4vw;
-    margin-bottom: 4vw;
-  }
-  .pz-intro {
-    font-size: 20px;
-    font-size: 1rem;
-  }
-  .pitanje-zdravlja .article h2 {
-    font-size: 22px;
-    font-size: 1.1rem;
-  }
-  .pitanje-zdravlja .article-big h2,
-  .pitanje-zdravlja .article-featured h2 {
-    font-size: 26px;
-    font-size: 1.3rem;
-  }
-  .pitanje-zdravlja .article img {
-    border-radius: 20px;
-  }
-  .pitanje-zdravlja .article-featured img {
-    margin-bottom: 0px;
-    margin-top: 4vw;
-  }
-  .pitanje-zdravlja .article h4 {
-    margin-bottom: 0px;
-    font-size: 15px;
-    font-size: 0.75rem;
-  }
-  .pitanje-zdravlja
-    .intro-block
-    .third:nth-child(3)
-    .article-featured:first-child
-    .article-pad {
-    border-bottom: none;
-  }
-  .pitanje-zdravlja .native-signature {
-    margin-top: 48px;
-  }
-  .pitanje-zdravlja .native-signature p {
-    margin-top: 16px;
-    margin-bottom: 48px;
-  }
-}
-</style>
