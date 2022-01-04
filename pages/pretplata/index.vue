6c00406960daf5290dd342cef2d32be2e26991c3
Marko Banušić <mbanusic@gmail.com>
2022-01-04 09:50:20 +0100

    pretplata update

--- pages/pretplata/index.vue
+++ pages/pretplata/index.vue
@@ -18,61 +18,6 @@
           </p>
         </div>
       </div>
-      <div class="full flex relative pretplata-sale">
-        <div
-          class="container center relative mobile-side-pad pretplata-packs pretplata-packboxes"
-        >
-          <div
-            class="full flex-responsive flex relative pretplata-packbox sale-packbox stretch"
-          >
-            <img
-              class="ps-xmas1"
-              src="@/assets/img/tg_xmas_1.png"
-              aria-hidden="true"
-            />
-            <img
-              class="ps-xmas2"
-              src="@/assets/img/tg_xmas_2.png"
-              aria-hidden="true"
-            />
-            <div class="full flex overtitle-parent">
-              <h3 class="overtitle">Božićna akcija</h3>
-            </div>
-            <h2 class="full"><span>49kn</span>1kn</h2>
-            <h4 class="full center-text undertitle">
-              Prvi mjesec, poslije 49kn mjesečno
-            </h4>
-            <div class="full pretplata-benefits">
-              <p class="full animate">
-                <font-awesome-icon :icon="['fas', 'check']"></font-awesome-icon>
-                prvi mjesec za samo 1 kunu
-              </p>
-              <p class="full animate">
-                <font-awesome-icon :icon="['fas', 'check']"></font-awesome-icon>
-                mjesečna Standard pretplata
-              </p>
-              <p class="full animate">
-                <font-awesome-icon :icon="['fas', 'check']"></font-awesome-icon>
-                neograničeno čitanje Telegrama
-              </p>
-              <p class="full animate">
-                <font-awesome-icon :icon="['fas', 'check']"></font-awesome-icon>
-                specijalni newsletteri
-              </p>
-              <p class="full animate">
-                <font-awesome-icon :icon="['fas', 'check']"></font-awesome-icon>
-                možete otkazati bilo kada
-              </p>
-              <h4 class="full center-text undertitle">
-                *promotivna ponuda vrijedi samo za nove pretplatnike
-              </h4>
-            </div>
-            <div class="full center btn-parent" @click="checkout(promo)">
-              <div class="btn animate">Odaberi</div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="full flex">
         <div class="container relative flex pretplata-packs mobile-side-pad">
           <input
@@ -768,6 +713,21 @@ export default {
   },
   methods: {
     checkout(termId) {
+      this.$gtm.push({ ecommerce: null }) // Clear the previous ecommerce object.
+      this.$gtm.push({
+        event: 'productClick',
+        ecommerce: {
+          click: {
+            products: [
+              {
+                name: this.terms[termId].title,
+                id: termId,
+                price: this.terms[termId].price,
+              },
+            ],
+          },
+        },
+      })
       if (this.$store.state.user.token) {
         this.checkout2(termId, -1)
       } else {
