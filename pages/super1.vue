6947850f378a9af0ab98418a5318b8a3e3168f02
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-03-14 15:54:46 +0100

    S2 revamped layout + mobile

--- pages/super1.vue
+++ pages/super1.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="main-container flex fancy-rubrika homepage superone">
+    <!-- TG Multiverse header -->
     <div class="full flex">
       <theader></theader>
     </div>
+    <!-- Portal header: logo i kategorije -->
     <div class="full relative cantha-header">
       <div class="container flex relative column-top-pad column-horizontal-pad">
         <div class="full center relative">
@@ -43,40 +45,13 @@
       </div>
       <div class="full header-filler"></div>
     </div>
+    <!-- Koljeno S1 -->
     <div class="full relative super1-block">
       <div class="block-title desktop-only full mobile-side-pad">
-        <div class="container flex relative column-full-pad">
-          <!--<div class="menu flex" role="menu">
-            <app-link role="menuitem" to="/look">Look</app-link>
-            <app-link role="menuitem" to="/snaga">Snaga</app-link>
-            <app-link role="menuitem" to="/relax">Relax</app-link>
-            <app-link role="menuitem" to="/emocije">Emocije</app-link>
-          </div>
-          <div class="flex">
-            <client-only>
-              <a
-                v-show="canLogIn"
-                class="newbtn newbtn-empty"
-                @click.prevent="login"
-                >Prijava</a
-              >
-              <a
-                v-show="!canLogIn"
-                class="newbtn newbtn-empty"
-                @click.prevent="logout"
-                >Odjava</a
-              >
-              <app-link
-                v-show="!$store.state.user.access"
-                to="/pretplata"
-                class="newbtn"
-                >Pretplatite se</app-link
-              >
-            </client-only>
-          </div>-->
-        </div>
+        <div class="container flex relative column-full-pad"></div>
       </div>
     </div>
+    <!-- Veliki blok - intro -->
     <div class="full relative">
       <div
         class="container elevate-over-section flex relative stretch standard-block intro-block"
@@ -84,24 +59,13 @@
         <div class="full flex">
           <div class="full flex mobile-side-pad stretch">
             <div
-              class="two-thirds column-horizontal-pad flex flex-responsive column-right-border"
+              class="full column-horizontal-pad flex split-articles big-split alt-big-break center-text"
             >
-              <featured
+              <standard
                 v-for="post in posts.slice(0, 1)"
                 :key="'super-' + post.id"
                 :post="post"
-              ></featured>
-            </div>
-            <div
-              class="third column-horizontal-pad flex flex-responsive split-articles super-sidebar"
-            >
-              <div
-                v-for="post in posts.slice(1, 4)"
-                :key="'super-' + post.id"
-                class="full column-vertical-pad stretch column-bottom-border"
-              >
-                <medium :post="post"></medium>
-              </div>
+              ></standard>
             </div>
           </div>
         </div>
@@ -110,7 +74,7 @@
             <div class="full column-top-border"></div>
           </div>
           <div
-            v-for="post in posts.slice(4, 7)"
+            v-for="post in posts.slice(2, 5)"
             :key="'super-' + post.id"
             class="third flex-responsive column-right-border stretch"
           >
@@ -121,6 +85,7 @@
         </div>
       </div>
     </div>
+    <!-- Prekid #1: shopping -->
     <div class="full flex">
       <div class="container flex relative stretch mobile-side-pad">
         <div class="full column-horizontal-pad column-top-pad">
@@ -130,7 +95,7 @@
           Shopping vodič
         </h3>
         <div
-          v-for="post in posts.slice(1, 5)"
+          v-for="post in posts.slice(3, 7)"
           :key="post.id"
           class="fourth flex-responsive column-horizontal-pad column-right-border"
         >
@@ -138,6 +103,7 @@
         </div>
       </div>
     </div>
+    <!-- Rubrika #1 -->
     <div class="full flex">
       <div class="container flex relative stretch mobile-side-pad">
         <div class="full column-horizontal-pad column-top-pad">
@@ -175,34 +141,68 @@
         </div>
       </div>
     </div>
+    <!-- Specijal #1: super preporuka -->
+    <div class="full relative">
+      <div
+        class="container mobile-side-pad flex relative stretch column-bottom-pad"
+      >
+        <div class="full column-horizontal-pad column-top-pad">
+          <div class="full cantha-separator"></div>
+        </div>
+        <h3 class="full center-text column-full-pad subsection-title">
+          Super preporuka
+        </h3>
+        <div
+          class="three-fourths flex-responsive column-horizontal-pad flex split-articles column-right-border big-split alt-big-break"
+        >
+          <medium
+            v-for="post in posts.slice(0, 1)"
+            :key="'super-' + post.id"
+            :post="post"
+          ></medium>
+        </div>
+        <div
+          class="fourth flex-responsive column-horizontal-pad mini-article-list"
+        >
+          <mini
+            v-for="post in posts.slice(2, 6)"
+            :key="post.id"
+            :post="post"
+          ></mini>
+        </div>
+      </div>
+    </div>
+    <!-- Prekid #2: native -->
     <div class="full flex">
       <div class="container flex relative stretch mobile-side-pad">
         <div class="full column-horizontal-pad column-top-pad">
           <div class="full cantha-separator"></div>
         </div>
         <h3 class="full center-text column-full-pad subsection-title">
-          SUPER preporuka
+          Od naših partnera
         </h3>
         <div
-          class="full column-horizontal-pad flex split-articles big-split alt-big-break center-text"
+          v-for="post in posts.slice(1, 5)"
+          :key="post.id"
+          class="fourth flex-responsive column-horizontal-pad column-right-border"
         >
-          <standard
-            v-for="post in posts.slice(2, 3)"
-            :key="'super-' + post.id"
-            :post="post"
-          ></standard>
+          <standard :post="post"></standard>
         </div>
       </div>
     </div>
+    <!-- Rubrika #2 -->
     <div class="full flex">
       <div class="container flex relative stretch mobile-side-pad">
-        <div class="full column-full-pad">
-          <div class="full column-top-border"></div>
+        <div class="full column-horizontal-pad column-top-pad">
+          <div class="full cantha-separator"></div>
         </div>
+        <h3 class="full center-text column-full-pad subsection-title">
+          Još SUPER priča
+        </h3>
         <div class="three-fourths flex-responsive flex stretch">
           <div class="half flex-responsive flex column-horizontal-pad">
             <featured
-              v-for="post in posts.slice(0, 1)"
+              v-for="post in posts.slice(1, 2)"
               :key="'super-' + post.id"
               :post="post"
             ></featured>
@@ -211,7 +211,7 @@
             class="half super-sidebar flex column-horizontal-pad column-left-border column-right-border flex-responsive split-articles"
           >
             <div
-              v-for="post in posts.slice(1, 4)"
+              v-for="post in posts.slice(2, 5)"
               :key="'super-' + post.id"
               class="full column-vertical-pad column-bottom-border stretch"
             >
@@ -228,6 +228,59 @@
         </div>
       </div>
     </div>
+    <!-- Specijal #2: izbor urednika -->
+    <div class="full flex">
+      <div class="container flex relative stretch mobile-side-pad">
+        <div class="full column-horizontal-pad column-top-pad">
+          <div class="full cantha-separator"></div>
+        </div>
+        <h3 class="full center-text column-full-pad subsection-title">
+          Izbor urednika
+        </h3>
+        <div
+          class="full column-horizontal-pad flex split-articles big-split alt-big-break center-text"
+        >
+          <standard
+            v-for="post in posts.slice(2, 3)"
+            :key="'super-' + post.id"
+            :post="post"
+          ></standard>
+        </div>
+        <div class="full flex stretch">
+          <div class="full flex column-full-pad">
+            <div class="full column-top-border"></div>
+          </div>
+          <div
+            v-for="post in posts.slice(3, 6)"
+            :key="'super-' + post.id"
+            class="third flex-responsive column-right-border stretch"
+          >
+            <div class="full flex column-horizontal-pad">
+              <featured :post="post"></featured>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Prekid #3: trend -->
+    <div class="full flex">
+      <div class="container flex relative stretch mobile-side-pad">
+        <div class="full column-horizontal-pad column-top-pad">
+          <div class="full cantha-separator"></div>
+        </div>
+        <h3 class="full center-text column-full-pad subsection-title">
+          Trend vodič
+        </h3>
+        <div
+          v-for="post in posts.slice(1, 5)"
+          :key="post.id"
+          class="fourth flex-responsive column-horizontal-pad column-right-border"
+        >
+          <standard :post="post"></standard>
+        </div>
+      </div>
+    </div>
+    <!-- Load more -->
     <div class="full flex">
       <div
         class="container flex column-top-pad relative stretch mobile-side-pad"
