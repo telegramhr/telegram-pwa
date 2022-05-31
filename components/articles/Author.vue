e27e1e1dddf324b175316ab6f960f02c56ea7c82
Marko Banušić <mbanusic@gmail.com>
2022-05-31 09:19:26 +0200

    fix authors widget

--- components/articles/Author.vue
+++ components/articles/Author.vue
@@ -2,26 +2,6 @@
   <div
     class="fourth flex column-horizontal-pad stretch new-author-tile column-right-border"
   >
-    <!--
-      <app-link :to="post.permalink" class="full flex fancy-author-tile">
-        <img :src="author.image" :alt="author.name" />
-        <div class="full flex fancy-author-content">
-          <h2 class="fancy-author-name">{{ author.name }}</h2>
-          <h3 class="fancy-author-title faded">
-            {{ author.byline }}
-          </h3>
-          <p class="fancy-article-preview">
-            {{ post.portal_title }}
-          </p>
-          <h5 class="article-meta">
-            <span class="meta-date">{{ post.time | parseTime }}</span>
-            <span v-if="post.recommendations" class="meta-preporuke"
-              >{{ post.recommendations }} preporuka</span
-            >
-          </h5>
-        </div>
-      </app-link>
-    -->
     <app-link
       class="full flex article-featured article relative"
       :to="post.permalink"
@@ -32,7 +12,7 @@
       <img :src="author.image" :alt="author.name" />
       <div class="full flex article-pad">
         <div class="full flex desktop-only">
-          <h3 class="overtitle">{{ post.authors[0].name }}</h3>
+          <h3 class="overtitle">{{ author.name }}</h3>
         </div>
         <h2 :id="'standard-' + post.id" class="full">
           {{ post.portal_title }}
@@ -60,10 +40,9 @@ export default {
   },
   computed: {
     author() {
-      if (this.post.authors[0].newsletter_list) {
-        return this.post.authors[0]
-      }
-      return this.post.authors[1]
+      return this.post.authors
+        .filter((author) => author.newsletter_list)
+        .shift()
     },
   },
 }
