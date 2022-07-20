f81cbbe56bf10901f08a9def5e242021f51c4a93
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-07-20 13:08:29 +0200

    Move Bili libar down

--- pages/pretplata/index.vue
+++ pages/pretplata/index.vue
@@ -7,48 +7,6 @@
         headline="Pretplatite se i podržite naše bespoštedno novinarstvo."
       ></theader>
     </div>
-    <a
-      href="#pretplata-selector"
-      class="full dark-element book-pretplata-promo relative"
-    >
-      <div class="container relative flex mobile-side-pad">
-        <div class="half column-full-pad center flex-responsive">
-          <div class="full flex article">
-            <h3 class="full overtitle">Specijalna ponuda</h3>
-            <h1 class="full">Telegram <br />+ Bili libar</h1>
-            <h4 class="full">
-              Nova knjiga Borisa Dežulovića,<br />
-              po posebnoj cijeni uz pretplatu
-            </h4>
-          </div>
-        </div>
-        <div class="half stretch flex flex-responsive">
-          <div class="half column-top-pad center flex-responsive">
-            <img
-              src="@/assets/img/tg_knjiga_vizual_dezulovic_pretplata.png"
-              alt="Portret Borisa Dežulovića i naslovnica njegove knjige 'Bili Libar'"
-            />
-          </div>
-          <div class="half center column-horizontal-pad flex-responsive">
-            <div class="full column-left-pad flex article">
-              <h2 class="full relative center-text">
-                <span class="faded strikethrough">149 kn</span> 99 kn
-              </h2>
-              <h2 class="full relative center-text">
-                ekskluzivna prednarudžba za pretplatnike
-              </h2>
-              <div class="full center">
-                <div class="newbtn huge-newbtn">Pretplatite se</div>
-              </div>
-              <h5 class="full center-text">
-                Odaberite jednu od opcija pretplate na Telegram, nakon čega
-                novog Dežulovića možete kupiti po specijalnoj cijeni.
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </a>
     <div id="pretplata-selector" class="full flex pretplata-page-content">
       <div class="full flex relative">
         <!--<div
@@ -255,6 +213,48 @@
             </div>
           </div>
         </div>
+        <a
+          href="#pretplata-selector"
+          class="full dark-element book-pretplata-promo relative"
+        >
+          <div class="container relative flex mobile-side-pad">
+            <div class="half column-full-pad center flex-responsive">
+              <div class="full flex article">
+                <h3 class="full overtitle">Specijalna ponuda</h3>
+                <h1 class="full">Telegram <br />+ Bili libar</h1>
+                <h4 class="full">
+                  Nova knjiga Borisa Dežulovića,<br />
+                  po posebnoj cijeni uz pretplatu
+                </h4>
+              </div>
+            </div>
+            <div class="half stretch flex flex-responsive">
+              <div class="half column-top-pad center flex-responsive">
+                <img
+                  src="@/assets/img/tg_knjiga_vizual_dezulovic_pretplata.png"
+                  alt="Portret Borisa Dežulovića i naslovnica njegove knjige 'Bili Libar'"
+                />
+              </div>
+              <div class="half center column-horizontal-pad flex-responsive">
+                <div class="full column-left-pad flex article">
+                  <h2 class="full relative center-text">
+                    <span class="faded strikethrough">149 kn</span> 99 kn
+                  </h2>
+                  <h2 class="full relative center-text">
+                    ekskluzivna prednarudžba za pretplatnike
+                  </h2>
+                  <div class="full center">
+                    <div class="newbtn huge-newbtn">Pretplatite se</div>
+                  </div>
+                  <h5 class="full center-text">
+                    Odaberite jednu od opcija pretplate na Telegram, nakon čega
+                    novog Dežulovića možete kupiti po specijalnoj cijeni.
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
         <div class="full flex relative">
           <div class="container relative flex">
             <div
