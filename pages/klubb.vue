5649aea45a1d5fdf945d69f97323716eea1b9c10
Marko Banušić <mbanusic@gmail.com>
2022-09-06 09:42:57 +0200

    fix qr code

--- pages/klubb.vue
+++ pages/klubb.vue
@@ -1477,41 +1477,43 @@
           Vaša klub iskaznica
         </h3>
         <div class="full center relative">
-          <div class="klub-card flex stretch relative">
-            <div class="full flex center">
-              <img
-                src="@/assets/img/telegram_logo_white.svg"
-                alt="Telegram logo"
-              />
-              <div class="full flex klub-card-content">
-                <p class="full center-text">
-                  {{ user.first_name }} {{ user.last_name }}
-                </p>
-                <p class="full center-text">ID {{ user.uid }}</p>
-                <p class="full center-text">
-                  Datum isteka:
-                  {{
-                    user.expiry_date
-                      ? new Date(user.expiry_date * 1000).toLocaleDateString(
-                          'hr-hr'
-                        )
-                      : 'Neograničeno'
-                  }}
-                </p>
+          <client-only>
+            <div class="klub-card flex stretch relative">
+              <div class="full flex center">
+                <img
+                  src="@/assets/img/telegram_logo_white.svg"
+                  alt="Telegram logo"
+                />
+                <div class="full flex klub-card-content">
+                  <p class="full center-text">
+                    {{ user.first_name }} {{ user.last_name }}
+                  </p>
+                  <p class="full center-text">ID {{ user.uid }}</p>
+                  <p class="full center-text">
+                    Datum isteka:
+                    {{
+                      user.expiry_date
+                        ? new Date(user.expiry_date * 1000).toLocaleDateString(
+                            'hr-hr'
+                          )
+                        : 'Neograničeno'
+                    }}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div class="full flex center klub-qr">
+              <div class="full flex center klub-qr">
+                <img
+                  v-if="user.uid"
+                  :src="`https://pretplate.telegram.hr/qrcode/${user.uid}`"
+                />
+              </div>
               <img
-                v-if="user.uid"
-                :src="`https://pretplate.telegram.hr/qrcode/${user.uid}`"
+                src="@/assets/img/tg_bg_fancyarc.jpg"
+                aria-hidden="true"
+                class="img-as-bg"
               />
             </div>
-            <img
-              src="@/assets/img/tg_bg_fancyarc.jpg"
-              aria-hidden="true"
-              class="img-as-bg"
-            />
-          </div>
+          </client-only>
         </div>
         <div class="full center">
           <p class="full faded center-text klub-disclaimer">
