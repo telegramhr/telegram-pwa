8a2579381a642251cb083f8c02e18e1daf7638a5
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-09-21 09:10:28 +0200

    Mini pretplata text fix

--- components/widgets/MiniPretplataNew.vue
+++ components/widgets/MiniPretplataNew.vue
@@ -4,7 +4,7 @@
     class="full flex stretch relative mini-pretplata mini-pretplata-new"
   >
     <p>
-      Bespoštedno novinarstvo ne finaniraju vlasti, bespoštedno novinarstvo
+      Bespoštedno novinarstvo ne financiraju vlasti, bespoštedno novinarstvo
       financiraju čitatelji. Podržite Telegram. Više o pretplati saznajte
       <span class="link-look-alike">ovdje.</span>
     </p>
