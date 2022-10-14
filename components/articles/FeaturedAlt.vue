fe84f0affe924cfa499349ab4d082207e6dda4e6
Marko Banušić <mbanusic@gmail.com>
2022-10-14 09:21:23 +0200

    fix tags

--- components/articles/FeaturedAlt.vue
+++ components/articles/FeaturedAlt.vue
@@ -9,12 +9,7 @@
       :tracker="post.trackerPermalink"
       :aria-labelledby="'featured-' + post.id"
     >
-      <img
-        :srcset="srcset"
-        :src="post.image.url"
-        :alt="post.image.alt"
-        loading="lazy"
-      />
+      <img :srcset="srcset" :src="post.image.url" :alt="post.image.alt" />
     </app-link>
     <div class="full flex article-pad">
       <app-link
