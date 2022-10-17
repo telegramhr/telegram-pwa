75f1f960d62df1f8fd9cadd53a80138a54b6f442
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-10-17 14:14:18 +0200

    Super 2 init

---
+++ components/articles/Superfeat.vue
@@ -0,0 +1,94 @@
+<template>
+  <div
+    class="full flex article-featured article-superfeat article"
+    role="article"
+  >
+    <app-link
+      class="full flex square-img"
+      :to="post.permalink"
+      :tracker="post.trackerPermalink"
+      :aria-labelledby="'featured-' + post.id"
+    >
+      <img
+        :srcset="srcset"
+        :src="post.image.url"
+        :alt="post.image.alt"
+        loading="lazy"
+      />
+    </app-link>
+    <div class="full flex article-pad mobile-side-pad">
+      <app-link
+        class="full flex"
+        :to="post.permalink"
+        :tracker="post.trackerPermalink"
+        :aria-labelledby="'featured-' + post.id"
+      >
+        <h2 :id="'featured-' + post.id" class="full animate">
+          <b>{{ parsedOvertitle }}</b> {{ post.portal_title }}
+        </h2>
+        <h5 class="full flex article-meta">
+          <span v-if="post.authors.length" class="meta-author"
+            ><i>Piše</i>
+            {{ post.authors.length ? post.authors[0].name : '' }}</span
+          >
+        </h5>
+      </app-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppLink from '@/components/AppLink'
+export default {
+  name: 'Superfeat',
+  components: { AppLink },
+  props: {
+    post: {
+      required: true,
+      type: Object,
+      default() {
+        return {
+          id: '',
+          related: [],
+          image: {
+            url: '',
+            alt: '',
+            author: '',
+          },
+          permalink: '',
+          title: '',
+          overtitle_tag: '',
+          subtitle: '',
+          category: '',
+          authors: [],
+          recommendations: 1,
+          alt: '',
+          time: 0,
+        }
+      },
+    },
+  },
+  data() {
+    return {
+      related: [],
+    }
+  },
+  computed: {
+    parsedOvertitle() {
+      return this.$options.filters.parseCat(
+        this.post.overtitle ? this.post.overtitle : this.post.category
+      )
+    },
+    srcset() {
+      let set = `${this.post.image.url}`
+      if (this.post.image.url2) {
+        set += `, ${this.post.image.url2} 2x`
+      }
+      if (this.post.image.url3) {
+        set += `, ${this.post.image.url3} 3x`
+      }
+      return set
+    },
+  },
+}
+</script>
