02a1668d07ca556130f4ad21026f29aee8b78dba
Marko Banušić <mbanusic@gmail.com>
2022-12-19 14:35:41 +0100

    remove qatar

--- pages/index.vue
+++ pages/index.vue
@@ -199,12 +199,6 @@
         <div class="full center m-order-6">
           <ad-unit id="telegram_desktop_billboard_v2"></ad-unit>
         </div>
-        <!-- qatar -->
-        <div class="full relative m-order-7">
-          <div class="container flex relative">
-            <category-big slug="qatar"></category-big>
-          </div>
-        </div>
         <!-- Teme -->
         <div class="full relative m-order-8 mobile-side-pad">
           <div class="full column-horizontal-pad column-top-pad">
