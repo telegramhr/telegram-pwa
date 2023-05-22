c22ad4555f85cb2bb37c452b4fadae9858f46182
Marko Banušić <mbanusic@gmail.com>
2023-05-22 09:38:06 +0200

    move adsense to plugin

--- plugins/adsense.client.js
+++ plugins/adsense.client.js
@@ -1,8 +1,17 @@
 export default ({ app, store }) => {
-  if (!store.state.user.access !== 'BR92VTWM') {
-    const adsbygoogle = window.adsbygoogle || []
-    adsbygoogle.push({
-      google_ad_client: 'ca-pub-2317149376955370',
-    })
+  console.log(store.state.user.access)
+  if (store.state.user.access !== 'BR92VTWM') {
+    const s = document.createElement('script')
+    s.src =
+      'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2317149376955370'
+    s.async = true
+    s.crossOrigin = 'anonymous'
+    document.head.appendChild(s)
+    const sovrn = document.createElement('script')
+    sovrn.src =
+      'https://get.s-onetag.com/71db7e62-a54b-43f0-98c7-ae8594228b02/tag.min.js'
+    sovrn.async = true
+    sovrn.defer = true
+    document.head.appendChild(sovrn)
   }
 }
