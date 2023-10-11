c3f353d285b6df8caaa2be348461114bfafe3c05
Marijan Mikolić <marijan.mikolic@gmail.com>
2023-10-11 11:06:35 +0200

    New footer

--- components/Elements/Tfooter.vue
+++ components/Elements/Tfooter.vue
@@ -23,7 +23,7 @@
         </div>
       </div>
     </div>-->
-    <app-link
+    <!--<app-link
       v-show="
         !$store.state.user.access &&
         !(
@@ -68,132 +68,140 @@
           </div>
         </div>
       </div>
-    </app-link>
-    <footer class="full flex mobile-side-pad">
-      <div class="container">
-        <div class="full flex center">
-          <div class="two-thirds main-footer-logo center">
-            <img
-              src="@/assets/img/telegram_logo_white.svg"
-              alt="Telegram logo"
-              loading="lazy"
-            />
+    </app-link>-->
+    <footer class="full flex">
+      <div class="container flex">
+        <div
+          class="full flex column-horizontal-pad column-mini-bottom-pad relative mobile-side-pad mobile-bottom-pad"
+        >
+          <div class="full center footer-logo-el">
+            <div class="fifth column-right-pad flex tablet-half mobile-half">
+              <img
+                src="@/assets/img/telegram_logo_black.svg"
+                alt="Telegram logo"
+                class="light-mode-only"
+                loading="lazy"
+              />
+              <img
+                src="@/assets/img/telegram_logo_black.svg"
+                alt="Telegram logo"
+                class="contrast-mode-only"
+                loading="lazy"
+              />
+              <img
+                src="@/assets/img/telegram_logo_white.svg"
+                alt="Telegram logo"
+                class="dark-mode-only"
+                loading="lazy"
+              />
+            </div>
           </div>
         </div>
-        <div class="full flex center">
-          <div class="full logo-line flex">
-            <app-link to="/super1" class="center animate">
-              <img
-                src="@/assets/img/s1_logo_clean_noline_white.svg"
-                alt="Super1 logo"
-                loading="lazy"
-              />
-            </app-link>
-            <a
-              href="https://telesport.telegram.hr"
-              target="_blank"
-              class="center animate"
-            >
-              <img
-                src="@/assets/img/telesport_logo_white.svg"
-                alt="Telesport logo"
-                loading="lazy"
-              />
-            </a>
-            <app-link to="/openspace" class="center animate">
-              <img
-                src="@/assets/img/openspace_logo_negative.svg"
-                alt="openspace logo"
-                loading="lazy"
-              />
-            </app-link>
-            <app-link to="/pitanje-zdravlja" class="center animate">
-              <img
-                src="@/assets/img/pz_logo_negative.svg"
-                alt="openspace logo"
-                loading="lazy"
-              />
-            </app-link>
-            <a
-              href="https://www.telegramgrupa.hr#studio"
-              target="_blank"
-              class="center animate"
-              rel="nofollow"
-            >
-              <img
-                src="@/assets/img/tg_studio_white.svg"
-                alt="TG Studio logo"
-                loading="lazy"
-              />
-            </a>
-          </div>
+        <div
+          class="fifth column-full-pad flex tablet-half mobile-half mobile-side-pad footer-list mobile-bottom-pad"
+        >
+          <p class="full bold column-mini-bottom-pad">Vijesti</p>
+          <app-link to="/vijesti" class="full">Vijesti</app-link>
+          <app-link to="/politika-kriminal" class="full"
+            >Politika&Kriminal</app-link
+          >
+          <app-link to="/komentar" class="full">Komentari</app-link>
+          <app-link to="/biznis-tech" class="full">Biznis&Tech</app-link>
+          <app-link to="/velike-price" class="full">Velike priče</app-link>
+          <app-link to="/zivot" class="full">Život</app-link>
+          <app-link to="/kultura" class="full">Kultura</app-link>
         </div>
-        <div class="full flex social-links center">
+        <div
+          class="fifth column-full-pad flex tablet-half mobile-half mobile-side-pad footer-list mobile-bottom-pad"
+        >
+          <p class="full bold column-mini-bottom-pad">Najnovije</p>
+
+          <app-link to="/tema/rat-u-ukrajini" class="full"
+            >Rat u Ukrajini</app-link
+          >
+          <app-link to="/tema/koronavirus" class="full">Koronavirus</app-link>
+          <app-link to="/tema/hrvatska" class="full">Hrvatska</app-link>
+          <app-link to="/tema/zagreb" class="full">Zagreb</app-link>
+          <app-link to="/tema/split" class="full">Split</app-link>
+          <app-link to="/tema/rijeka" class="full">Rijeka</app-link>
+          <app-link to="/tema/osijek" class="full">Osijek</app-link>
+        </div>
+        <div
+          class="fifth column-full-pad flex tablet-half mobile-half mobile-side-pad footer-list mobile-bottom-pad"
+        >
+          <p class="full bold column-mini-bottom-pad">Magazin</p>
+          <a href="https://telesport.telegram.hr/" class="full">Telesport</a>
+          <app-link to="/super1" class="full">Super1</app-link>
+          <app-link to="/pitanje-zdravlja" class="full"
+            >PitanjeZdravlja</app-link
+          >
+          <app-link to="/openspace" class="full">OpenSpace</app-link>
+        </div>
+        <div
+          class="fifth column-full-pad flex tablet-half mobile-half mobile-side-pad footer-list mobile-bottom-pad"
+        >
+          <p class="full bold column-mini-bottom-pad">Pratite nas</p>
           <a
             href="https://www.facebook.com/Telegram.hr/"
-            class="center animate"
-            aria-label="Facebook"
-            rel="noreferrer nofollow"
+            target="_blank"
+            class="full"
+            >Facebook</a
           >
-            <font-awesome-icon :icon="['fab', 'facebook-f']"></font-awesome-icon
-          ></a>
           <a
-            href="https://twitter.com/telegramhr/"
-            class="center animate"
-            aria-label="Twitter"
-            rel="noreferrer nofollow"
+            href="https://www.youtube.com/@TelegramhrVideo"
+            target="_blank"
+            class="full"
+            >YouTube</a
           >
-            <font-awesome-icon :icon="['fab', 'twitter']"></font-awesome-icon
-          ></a>
           <a
             href="https://www.instagram.com/telegram.hr/"
-            class="center animate"
-            aria-label="Instagram"
-            rel="noreferrer nofollow"
-          >
-            <font-awesome-icon :icon="['fab', 'instagram']"></font-awesome-icon
-          ></a>
-          <app-link
-            to="https://www.telegram.hr/feed"
-            class="center animate"
-            aria-label="RSS"
-          >
-            <font-awesome-icon :icon="['fas', 'rss']"></font-awesome-icon
-          ></app-link>
-        </div>
-        <div class="full flex services center">
-          <app-link to="/pretplata" class="animate">Pretplata</app-link>
-          <app-link to="/stranica/impressum" class="animate"
-            >Impressum</app-link
-          >
-          <app-link to="/stranica/oglasavanje" class="animate"
-            >Oglašavanje</app-link
-          >
-          <app-link to="/stranica/kontaktirajte-nas" class="animate"
-            >Kontakt</app-link
-          >
-          <app-link to="/stranica/uvjeti-koristenja" class="animate"
-            >Uvjeti korištenja</app-link
-          >
-          <app-link to="/stranica/pravila-privatnosti" class="animate"
-            >Pravila o privatnosti</app-link
+            target="_blank"
+            class="full"
+            >Instagram</a
           >
           <a
-            v-if="$route.name === 'openspace'"
-            href="https://hr.jooble.org/"
+            href="https://www.tiktok.com/@telegram.hr"
             target="_blank"
+            class="full"
+            >TikTok</a
           >
-            Jooble oglasi
-          </a>
+          <a href="https://twitter.com/telegramhr/" target="_blank" class="full"
+            >Twitter</a
+          >
+          <a href="https://www.telegram.hr/feed" target="_blank" class="full"
+            >RSS</a
+          >
         </div>
-        <div class="full center-text conversion-notice">
-          Stopa konverzije kune u euro određena je na Vijeću za ekonomske i
-          financijske poslove EU-a, 12.srpnja 2022., po središnjem paritetu, te
-          iznosi 1 euro = 7,53450 kuna.
+        <div
+          class="fifth column-full-pad flex tablet-half mobile-half mobile-side-pad footer-list mobile-bottom-pad"
+        >
+          <p class="full bold column-mini-bottom-pad">Plus</p>
+
+          <app-link to="/pretplata" class="full">Pretplata </app-link>
+          <app-link to="/dojave" class="full">Pošaljite nam vijest</app-link>
+          <app-link to="/stranica/impressum" class="full">Impressum</app-link>
+          <app-link to="/stranica/oglasavanje" class="full"
+            >Oglašavanje</app-link
+          >
+          <app-link to="/stranica/kontakt" class="full">Kontakt </app-link>
+          <app-link to="/stranica/uvjeti-koristenja" class="full"
+            >Uvjeti korištenja</app-link
+          >
+          <app-link to="/stranica/pravila-privatnosti" class="full"
+            >Pravila privatnosti</app-link
+          >
         </div>
-        <div class="copyright full center-text">
-          Sva prava pridržana &copy; {{ new Date().getFullYear() }} Telegram
-          Media Grupa d.o.o.
+        <div class="full flex column-horizontal-pad mobile-side-pad">
+          <div class="full flex column-top-pad column-top-border"></div>
+          <div class="full center-text conversion-notice mobile-top-pad">
+            Stopa konverzije kune u euro određena je na Vijeću za ekonomske i
+            financijske poslove EU-a, 12.srpnja 2022., po središnjem paritetu,
+            te iznosi 1 euro = 7,53450 kuna.
+          </div>
+          <div class="copyright full center-text">
+            Sva prava pridržana &copy; {{ new Date().getFullYear() }} Telegram
+            Media Grupa d.o.o.
+          </div>
         </div>
       </div>
     </footer>
