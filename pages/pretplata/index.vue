35991a99de5880bdf638c1b48a4955d5e276e965
Marko Banušić <mbanusic@gmail.com>
2023-11-29 13:49:56 +0100

    fix race condition

--- pages/pretplata/index.vue
+++ pages/pretplata/index.vue
@@ -1066,15 +1066,20 @@ export default {
         this.checkout2(termId, -1)
       } else {
         const _that = this
-        window.tp.pianoId.show({
-          screen: 'register',
-          width: window.innerWidth > 720 ? 600 : 375,
-          loggedIn(data) {
-            _that.$store.dispatch('user/setUser', data.user)
-            // window.location.reload()
-            _that.checkout2(termId, -2)
+        window.tp.push([
+          'init',
+          () => {
+            window.tp.pianoId.show({
+              screen: 'register',
+              width: window.innerWidth > 720 ? 600 : 375,
+              loggedIn(data) {
+                _that.$store.dispatch('user/setUser', data.user)
+                // window.location.reload()
+                _that.checkout2(termId, -2)
+              },
+            })
           },
-        })
+        ])
       }
     },
     checkout2(termId, back) {
