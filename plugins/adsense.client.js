c34f185e1c7f5f98cbc6d54b778ab1dc62ce3f18
Marko Banušić <mbanusic@gmail.com>
2024-01-02 14:38:59 +0100

    new prebid

--- plugins/adsense.client.js
+++ plugins/adsense.client.js
@@ -20,44 +20,19 @@ export default ({ app, store }) => {
     g.async = true
     g.defer = true
     document.head.appendChild(g)
-
-    const i = Math.floor(Math.random() * 2)
-    if (i) {
-      const sovrn = document.createElement('script')
-      sovrn.src =
-        'https://get.s-onetag.com/71db7e62-a54b-43f0-98c7-ae8594228b02/tag.min.js'
-      sovrn.async = true
-      sovrn.defer = true
-      document.head.appendChild(sovrn)
-      app.$gtm.push({
-        event: 'reload-script',
-        provider: 'sovrn',
-      })
-    } else {
-      const defractal = document.createElement('script')
-      defractal.src =
-        'https://cdn.defractal.com/scripts/defractal-7-00770051-F494-429B-A1AF-2D4AB7F53D99js'
-      document.head.appendChild(defractal)
-      app.$gtm.push({
-        event: 'reload-script',
-        provider: 'defractal',
-      })
-    }
-    const j = Math.floor(Math.random() * 2)
-    if (j) {
-      window.prebid = 'prebid'
-      const prebid = document.createElement('script')
-      prebid.src = '/prebid7.40.0.js'
-      prebid.async = true
-      prebid.defer = true
-      document.head.appendChild(prebid)
-    } else {
-      window.prebid = 'rubicon'
-      const prebid = document.createElement('script')
-      prebid.src = '//micro.rubiconproject.com/prebid/dynamic/26072.js'
-      prebid.async = true
-      prebid.defer = true
-      document.head.appendChild(prebid)
-    }
+    const defractal = document.createElement('script')
+    defractal.src =
+      'https://cdn.defractal.com/scripts/defractal-7-00770051-F494-429B-A1AF-2D4AB7F53D99js'
+    document.head.appendChild(defractal)
+    app.$gtm.push({
+      event: 'reload-script',
+      provider: 'defractal',
+    })
+    window.prebid = 'rubicon'
+    const prebid = document.createElement('script')
+    prebid.src = '//micro.rubiconproject.com/prebid/dynamic/26072.js'
+    prebid.async = true
+    prebid.defer = true
+    document.head.appendChild(prebid)
   }
 }
