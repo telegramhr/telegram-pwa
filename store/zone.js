c83c7673132c39a9aae6fc3a773703276a5ad746
Marko Banušić <mbanusic@gmail.com>
2024-01-22 11:07:43 +0100

    fix load

--- store/zone.js
+++ store/zone.js
@@ -9,15 +9,15 @@ export const state = () => ({
 
 export const mutations = {
   setPosts(state, data) {
-    state.posts = data
-    state.updated = new Date().getTime()
+    state[data.zone].posts = data.data
+    state[data.zone].updated = new Date().getTime()
   },
 }
 
 export const actions = {
   pullPosts({ commit, dispatch, state }, payload) {
     return new Promise((resolve) => {
-      if (state.updated + 60 * 1000 < new Date().getTime()) {
+      if (state[payload.zone].updated + 60 * 1000 < new Date().getTime()) {
         this.$axios.get('/api/zone/' + payload.zone).then((res) => {
           commit('setPosts', { data: res.data, zone: payload.zone })
           dispatch('posts/setPosts', res.data, { root: true })
