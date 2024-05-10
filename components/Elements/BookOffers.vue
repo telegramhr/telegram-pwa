71152492fca4f1e652f481cd42b14cef637682a8
Marijan Mikolić <marijan.mikolic@gmail.com>
2024-05-10 10:34:54 +0200

    Replace single book widget with multibook widget

--- components/Elements/BookOffers.vue
+++ components/Elements/BookOffers.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="container cantha-small-block flex relative native-block offers-widget column-top-pad stretch mobile-side-pad"
+    class="container cantha-small-block flex relative native-block offers-widget column-vertical-pad stretch mobile-side-pad"
   >
     <div class="full column-horizontal-pad">
       <div class="full cantha-separator"></div>
@@ -9,31 +9,24 @@
       <div
         class="noththree full center-text column-vertical-pad subsection-title"
       >
-        <img
-          src="@/assets/img/telegram_logo_black.svg"
-          alt="Telegram logo"
-          loading="lazy"
-        />
-        knjige
+        Telegram knjige
       </div>
     </div>
     <div class="full flex stretch book-offers-widget">
       <div class="fourth flex-responsive column-horizontal-pad">
         <app-link
-          to="/knjiga/sanja-modric-igla-pila-ravnalo"
+          to="/knjiga/boris-dezulovic-bili-libar-2"
           class="full flex article relative"
         >
           <img
-            src="@/assets/img/tg_book_clean_modric.png"
-            alt="Naslovnica nove knjige Sanje Modrić 'Igla, pila, ravnalo'"
+            src="@/assets/img/tg_book_clean_dezulovic2.png"
+            alt="Naslovnica knjige Borisa Dežulovića 'Bili libar 2'"
             loading="lazy"
           />
           <div class="full flex article-pad center-text">
-            <div class="full offer-title">Igla, pila, ravnalo</div>
-            <div class="nothfour full">Sanja Modrić</div>
-            <div class="noththree full overtitle">
-              <span class="faded strikethrough">19,99€</span> 13,99€
-            </div>
+            <div class="full offer-title">Bili libar 2</div>
+            <div class="nothfour full">Boris Dežulović</div>
+            <div class="noththree full overtitle">19,99€</div>
             <div class="full center">
               <div class="newbtn">Pogledajte više</div>
             </div>
@@ -42,19 +35,19 @@
       </div>
       <div class="fourth flex-responsive column-horizontal-pad">
         <app-link
-          to="/knjiga/boris-dezulovic-bili-libar"
+          to="/knjiga/aleksandar-stankovic-depra"
           class="full flex article relative"
         >
           <img
-            src="@/assets/img/tg_book_clean_dezulovic.png"
-            alt="Naslovnica knjige Borisa Dežulovića 'Bili libar'"
+            src="@/assets/img/tg_book_mockup_stankovic.png"
+            alt="Naslovnica nove knjige Aleksandra Stankovića 'Depra'"
             loading="lazy"
           />
           <div class="full flex article-pad center-text">
-            <div class="full offer-title">Bili libar</div>
-            <div class="nothfour full">Boris Dežulović</div>
+            <div class="full offer-title">Depra</div>
+            <div class="nothfour full">Aleksandar Stanković</div>
             <div class="noththree full overtitle">
-              <span class="faded strikethrough">19,78€</span> 13,14€
+              <span class="faded strikethrough">19,99€</span> 14,99€
             </div>
             <div class="full center">
               <div class="newbtn">Pogledajte više</div>
@@ -86,19 +79,19 @@
       </div>
       <div class="fourth flex-responsive column-horizontal-pad">
         <app-link
-          to="/knjiga/zrinka-paladino-o-zagrebu"
+          to="/knjiga/boris-dezulovic-bili-libar"
           class="full flex article relative"
         >
           <img
-            src="@/assets/img/tg_book_clean_paladino.png"
-            alt="Naslovnica knjige Zrinke Paladino 'O Zagrebu i... Telegramovih 48'"
+            src="@/assets/img/tg_book_clean_dezulovic.png"
+            alt="Naslovnica knjige Borisa Dežulovića 'Bili libar'"
             loading="lazy"
           />
           <div class="full flex article-pad center-text">
-            <div class="full offer-title">O Zagrebu i...</div>
-            <div class="nothfour full">Zrinka Paladino</div>
+            <div class="full offer-title">Bili libar</div>
+            <div class="nothfour full">Boris Dežulović</div>
             <div class="noththree full overtitle">
-              <span class="faded strikethrough">23,76€</span> 13,14€
+              <span class="faded strikethrough">19,78€</span> 13,14€
             </div>
             <div class="full center">
               <div class="newbtn">Pogledajte više</div>
--- pages/index.vue
+++ pages/index.vue
@@ -442,44 +442,10 @@
     <div class="full relative">
       <os-homepage></os-homepage>
     </div>
-    <!-- Book widget Bili libar 2 -->
-    <app-link
-      to="/knjiga/boris-dezulovic-bili-libar-2"
-      class="full flex relative center mobile-side-pad darkened-bg column-full-pad shoo-bottom"
-    >
-      <div class="container relative flex stretch">
-        <div class="forty tablet-full flex flex-responsive center">
-          <div class="full center-text newbook-overtitle">Telegram naklada</div>
-          <h2 class="full center-text newbook-title newbook-title-smaller">
-            BILI LIBAR 2
-          </h2>
-          <div class="full center-text newbook-author">Boris Dežulović</div>
-          <p class="full center-text newbook-intro">
-            Veliki Telegramov hit dobio je nastavak, s 12 još raskošnijih, još
-            čudesnijih i luđih priča o jednom nogometnom klubu. I, naravno, ne
-            samo o njemu.
-          </p>
-        </div>
-
-        <div class="forty tablet-full flex flex-responsive center">
-          <img
-            src="@/assets/img/tg_book_clean_dezulovic2.png"
-            alt="Naslovnica nove knjige Borisa Dežulovića 'BILI LIBAR 2'"
-          />
-        </div>
-        <div class="fifth tablet-full flex flex-responsive center">
-          <div class="full relative center-text newbook-price">19,99€</div>
-          <div class="full relative center-text neat-numbers">
-            (150,61 kn)
-            <br />
-            Besplatna dostava po cijeloj Hrvatskoj
-          </div>
-          <div class="full center btn-parent newbook-btn">
-            <div class="newbtn clickable huge-newbtn animate">Kupite</div>
-          </div>
-        </div>
-      </div>
-    </app-link>
+    <!-- Book widget -->
+    <div class="full relative">
+      <book-offers></book-offers>
+    </div>
     <!-- Rubrike -->
     <div class="full relative">
       <div class="container flex relative block-5 standard-block">
