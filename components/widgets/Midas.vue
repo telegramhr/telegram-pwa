a7ba93ec0d7e2720d4ef1f3ae55a17856462b156
Marko Banušić <mbanusic@gmail.com>
2024-05-13 18:06:23 +0200

    fix midas ids

--- components/widgets/Midas.vue
+++ components/widgets/Midas.vue
@@ -24,74 +24,84 @@ export default {
     return {
       ids: {
         vijesti: {
-          intext: 11430,
+          intext: '524-11430',
           'text-only': 11426,
           'standard-16': 11427,
           ecomm: 11429,
           'standard-4': 11428,
+          main: 468,
         },
         'politika-kriminal': {
-          intext: 11435,
+          intext: '524-11435',
           'text-only': 11431,
           'standard-16': 11432,
           ecomm: 11434,
           'standard-4': 11433,
+          main: 468,
         },
         'biznis-tech': {
-          intext: 11445,
+          intext: '524-11445',
           'text-only': 11441,
           'standard-16': 11442,
           ecomm: 11444,
           'standard-4': 11443,
+          main: 468,
         },
         kultura: {
-          intext: 11460,
+          intext: '524-11460',
           'text-only': 11456,
           'standard-16': 11457,
           ecomm: 11459,
           'standard-4': 11458,
+          main: 468,
         },
         zivot: {
-          intext: 11455,
+          intext: '524-11455',
           'text-only': 11451,
           'standard-16': 11452,
           ecomm: 11454,
           'standard-4': 11453,
+          main: 468,
         },
         'velike-price': {
-          intext: 11450,
+          intext: '524-11450',
           'text-only': 11446,
           'standard-16': 11447,
           ecomm: 11449,
           'standard-4': 11448,
+          main: 468,
         },
         komentari: {
-          intext: 11440,
+          intext: '524-11440',
           'text-only': 11436,
           'standard-16': 11437,
           ecomm: 11439,
           'standard-4': 11438,
+          main: 468,
         },
         openspace: {
-          intext: 11476,
+          intext: '524-11476',
           'text-only': 11472,
           'standard-16': 11473,
           ecomm: 11475,
           'standard-4': 11474,
+          main: 468,
         },
         'pitanje-zdravlja': {
-          intext: 11481,
+          intext: '524-11481',
           'text-only': 11477,
           'standard-16': 11478,
           ecomm: 11480,
           'standard-4': 11479,
+          main: 468,
         },
         super1: {
-          intext: 11471,
+          intext: '786-11471',
           'text-only': 11467,
           'standard-16': 11468,
           ecomm: 11470,
           'standard-4': 11469,
+          main: 469,
         },
       },
     }
@@ -118,21 +128,22 @@ export default {
           category = 'super1'
         }
         const script = document.createElement('script')
-        const ids = Object.keys(this.ids[this.$route.params.category])
+        const main = this.ids[category].main ?? ''
+        const ids = Object.keys(this.ids[category])
           .map((value) => {
-            if (value !== 'intext') {
+            if (value !== 'intext' && value !== 'main') {
               return this.ids[category][value]
             }
           })
           .join('-')
-        script.src = `https://cdn2.midas-network.com/Scripts/midasWidget-11-468${ids}.js`
+        script.src = `https://cdn2.midas-network.com/Scripts/midasWidget-11-${main}${ids}.js`
         script.async = true
         document
           .getElementById(`midasWidget__${this.id}`)
           .insertAdjacentElement('afterend', script)
         const script2 = document.createElement('script')
         const intext = this.ids[category].intext
-        script2.src = `https://cdn2.midas-network.com/Scripts/midasWidget-11-524-${intext}.js`
+        script2.src = `https://cdn2.midas-network.com/Scripts/midasWidget-11-${intext}.js`
         script2.async = true
         document
           .getElementById(`midasWidget__${this.id}`)
