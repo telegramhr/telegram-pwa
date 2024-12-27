375e50ed4939130f2b1ce5e62cfeca2f045d39a7
Marko Banušić <mbanusic@gmail.com>
2024-12-27 12:03:53 +0100

    dotmetrics

--- plugins/dotmetrics.client.js
+++ plugins/dotmetrics.client.js
@@ -1,41 +1,26 @@
 export default ({ route }, inject) => {
   function check(path) {
     let dotmetricsId = '1182'
-    if (path.includes('politika-kriminal')) {
-      dotmetricsId = '1174'
-    }
-    if (path.includes('biznis-tech')) {
-      dotmetricsId = '1176'
-    }
-    if (path.includes('kultura')) {
-      dotmetricsId = '1178'
-    }
-    if (path.includes('zivot')) {
-      dotmetricsId = '1179'
-    }
-    if (path.includes('velike-price')) {
-      dotmetricsId = '1177'
-    }
-    if (path.includes('pitanje-zdravlja')) {
-      dotmetricsId = '12607'
+    if (
+      path.includes('politika-kriminal') ||
+      path.includes('biznis-tech') ||
+      path.includes('komentari')
+    ) {
+      dotmetricsId = '15854'
     }
     if (
-      path.includes('openspace') ||
-      path.includes('vjestine') ||
-      path.includes('tvrtke-karijere') ||
-      path.includes('vodici') ||
-      path.includes('kvizovi-testovi')
+      path.includes('kultura') ||
+      path.includes('zivot') ||
+      path.includes('pitanje-zdravlja') ||
+      path.includes('velike-price')
     ) {
-      dotmetricsId = '12608'
+      dotmetricsId = '15855'
     }
     if (path.includes('super1') || path.includes('superone')) {
       dotmetricsId = '4136'
     }
-    if (path.includes('telesport')) {
+    if (path.includes('telesport') || path.includes('sport')) {
       dotmetricsId = '1175'
-    }
-    if (path === '/') {
-      dotmetricsId = '1173'
     }
     return dotmetricsId
   }
