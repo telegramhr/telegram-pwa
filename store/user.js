af6cf564c2f11cfedcaf13e771f33f826f918b59
Marko Banušić <mbanusic@gmail.com>
2025-01-21 11:36:43 +0100

    fix comments

--- store/user.js
+++ store/user.js
@@ -74,7 +74,7 @@ export const actions = {
         resolve(state.coral_token)
         return
       }
-      this.$axios.get(`/pretplate/coral/token2/${state.email}`).then((res) => {
+      this.$axios.get(`/pretplate/coral/token/${state.token}`).then((res) => {
         commit('setCoral', res.data)
         resolve(res.data)
       })
