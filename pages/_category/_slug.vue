44b56edd378aa6b523487d591ee5575edd52ef6a
Marko Banušić <mbanusic@gmail.com>
2025-04-10 12:45:21 +0200

    add more info to remp

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -883,6 +883,7 @@ export default {
         token: 'd4fa2928-7d6a-4f6c-ac95-1f5a1ddd1702',
         signedIn: !!this.$store.state.user.id,
         userId: this.$store.state.user.id.toString() ?? '',
+        userSubscribed: !!this.$store.state.user.access.length,
         cookieDomain: '.telegram.hr',
         storage: 'local_storage',
         storageExpiration: {
