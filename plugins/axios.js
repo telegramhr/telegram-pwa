3236e7a85a7810918791999edf49c9c8ce0dc736
Marko Banušić <mbanusic@gmail.com>
2025-06-04 11:38:34 +0200

    fix paths

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,12 +4,30 @@ export default function ({ app, $axios, redirect, store }) {
   $axios.onRequest((config) => {
     if (Capacitor.isNativePlatform()) {
       config.headers['X-Type'] = 'app'
-    }
-    if (config.url.startsWith('/api/')) {
-      config.url = config.url.replace(
-        '/api/',
-        'https://www.telegram.hr/wp-json/telegram/pwa/v1/'
-      )
+      if (config.url.startsWith('/api/')) {
+        config.url = config.url.replace(
+          '/api/',
+          'https://www.telegram.hr/wp-json/telegram/pwa/v1/'
+        )
+      }
+      if (config.url.startsWith('/crm/')) {
+        config.url = config.url.replace(
+          '/crm/',
+          'https://pretplata.telegram.hr/'
+        )
+      }
+      if (config.url.startsWith('/pretplate/')) {
+        config.url = config.url.replace(
+          '/pretplate/',
+          'https://pretplate.telegram.hr/'
+        )
+      }
+      if (config.url.startsWith('/mailer/')) {
+        config.url = config.url.replace(
+          '/mailer/',
+          'https://mailer.telegram.hr/'
+        )
+      }
     }
   })
 }
