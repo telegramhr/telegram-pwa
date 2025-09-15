8815eb1a6e2f73a335a189cfcccb7fc6dc4cc7e5
Darian J. <darian.jug@smartcode.eu>
2025-09-15 15:16:49 +0200

    More attempt of fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,18 @@ module.exports = {
       name: 'telegram-pwa',
       exec_mode: 'cluster',
       instances: process.env.PM2_INSTANCES || 2,
-      script: './node_modules/nuxt/bin/nuxt.js',
+      script: 'npm',
       args: 'start',
       cwd: '/app',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
         HOST: '0.0.0.0'
-      }
+      },
+      error_file: '/dev/stderr',
+      out_file: '/dev/stdout',
+      time: false,
+      max_memory_restart: '512M'
     }
   ]
 }
