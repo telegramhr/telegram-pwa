ab8c4389aad1cc2cbc6e600ee1a0e17103af1e0e
Marko Banušić <mbanusic@gmail.com>
2025-10-10 14:23:55 +0200

    disable auto signin

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,6 @@
         data-context="signin"
         data-client_id="345595854714-ikp5kmgn1thkccvlkerioqkeh5773ndr.apps.googleusercontent.com"
         :data-login_uri="`https://pretplata.telegram.hr/users/google/sign?url=https://www.telegram.hr${$router.fullPath}`"
-        data-auto_select="true"
         data-itp_support="true"
       ></div>
     </client-only>
