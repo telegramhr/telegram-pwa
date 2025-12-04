42e5988407cf039c4dc26c81325c71e8c94ce6da
Darian J. <darian.jug@smartcode.eu>
2025-12-04 12:05:58 +0100

    Added sentry

--- nuxt.config.js
+++ nuxt.config.js
@@ -156,6 +156,7 @@ export default {
     'cookie-universal-nuxt',
     '@nuxtjs/gtm',
     '@nuxtjs/onesignal',
+    '@nuxtjs/sentry',
   ],
 
   oneSignal: {
@@ -314,4 +315,28 @@ export default {
       })
     },
   },
+
+  // Sentry error tracking
+  sentry: {
+    dsn: 'https://47b01937396e0d7d1139e7eb75ff9b97@sentry.smartcode.rocks/4',
+    config: {
+      environment: process.env.NODE_ENV || 'development',
+    },
+    clientIntegrations: {
+      Dedupe: {},
+      ExtraErrorData: {},
+      ReportingObserver: {},
+      RewriteFrames: {},
+      Vue: {attachProps: true}
+    },
+    serverIntegrations: {
+      Dedupe: {},
+      ExtraErrorData: {},
+      RewriteFrames: {}
+    },
+    publishRelease: false,
+    sourceMapStyle: 'hidden-source-map',
+    lazy: false
+  },
 }
+
