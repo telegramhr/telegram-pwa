b27488250fd73485f18d2b76173105b27de81028
Marinko Sevo <marinko.sevo@telegram.hr>
2025-12-30 16:44:51 +0100

    Revert changes

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -73,23 +73,14 @@
               <client-only>
                 <span
                   v-if="
-                    this.$store.state.user.access?.length &&
-                    post.paywall === 'always'
+                    this.$store.state.user.access && post.paywall === 'always'
                   "
                   class="fancy-overtitle-premium"
                 >
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M2.75 8.05715H5.23557V7.54781C5.21519 6.52913 5.39856 5.83643 5.74491 5.46971C5.94864 5.2456 6.15238 5.2456 6.19312 5.2456H6.74321V12.2337H4.97071V14.6989H12.8756V12.2745H10.6142V5.26598H10.9402H11.0216C11.3272 5.26598 11.5514 5.34747 11.7551 5.55121C12.1218 5.91793 12.3052 6.56988 12.3459 7.58856L12.3663 8.07752H14.75V2.80078H2.75V8.05715Z"
-                      fill="#343434"
-                    />
-                  </svg>
+                  <img
+                    src="@/assets/img/t_logo.svg"
+                    alt="Telegram monogram logo (T)"
+                  />
                   Samo za pretplatnike
                 </span>
               </client-only>
@@ -145,23 +136,14 @@
                 <client-only
                   ><span
                     v-if="
-                      this.$store.state.user.access?.length &&
-                      post.paywall === 'always'
+                      this.$store.state.user.access && post.paywall === 'always'
                     "
                     class="fancy-overtitle-premium"
                   >
-                    <svg
-                      width="18"
-                      height="18"
-                      viewBox="0 0 18 18"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M2.75 8.05715H5.23557V7.54781C5.21519 6.52913 5.39856 5.83643 5.74491 5.46971C5.94864 5.2456 6.15238 5.2456 6.19312 5.2456H6.74321V12.2337H4.97071V14.6989H12.8756V12.2745H10.6142V5.26598H10.9402H11.0216C11.3272 5.26598 11.5514 5.34747 11.7551 5.55121C12.1218 5.91793 12.3052 6.56988 12.3459 7.58856L12.3663 8.07752H14.75V2.80078H2.75V8.05715Z"
-                        fill="#343434"
-                      />
-                    </svg>
+                    <img
+                      src="@/assets/img/t_logo.svg"
+                      alt="Telegram monogram logo (T)"
+                    />
                     Samo za pretplatnike
                   </span>
                 </client-only>
@@ -197,10 +179,7 @@
               <h2 class="full">
                 {{ post.subtitle | parseCat }}
               </h2>
-              <div
-                v-if="post.type === 'commentary'"
-                class="nothfive full flex relative article-meta mobile-only"
-              >
+              <div class="nothfive full flex relative article-meta mobile-only">
                 <app-link
                   v-for="author in post.authors"
                   :key="author.name"
@@ -223,119 +202,6 @@
                     :author="post.authors[0]"
                   ></subscribe-link>
                 </client-only>
-                <div class="commentary-meta-other">
-                  <time class="meta-date" :datetime="post.time">{{
-                    post.time | parseTime
-                  }}</time>
-                  <span
-                    v-if="post.recommendations"
-                    class="meta-preporuke"
-                    itemprop="interactionStatistics"
-                    >{{ post.recommendations }} preporuka</span
-                  >
-                </div>
-              </div>
-              <div
-                v-if="post.type !== 'noimage'"
-                class="nothfive full flex relative article-meta article-meta-nonCommentary"
-              >
-                <div class="meta-all">
-                  <div class="meta-author-all">
-                    <app-link
-                      v-for="author in post.authors"
-                      :key="author.name"
-                      :to="author.url"
-                      class="meta-author flex"
-                      ><img
-                        v-if="author.image"
-                        :src="author.image"
-                        :alt="author.name"
-                      /><span>Piše</span
-                      ><span
-                        class="vcard author"
-                        rel="author"
-                        itemprop="author"
-                        >{{ author.name }}</span
-                      ></app-link
-                    >
-                  </div>
-                  <div class="meta-other">
-                    <div class="flex">
-                      <time class="meta-date" :datetime="post.time">{{
-                        post.time | parseTime
-                      }}</time>
-                      <span
-                        v-if="post.recommendations"
-                        class="meta-preporuke"
-                        itemprop="interactionStatistics"
-                        >{{ post.recommendations }} preporuka</span
-                      >
-                    </div>
-                    <action-bar
-                      v-if="!post.audio"
-                      :class="{
-                        nonAudio: true,
-                        nonComments:
-                          post.category_slug.includes('superone') ||
-                          post.category_slug.includes('pitanje-zdravlja') ||
-                          post.category_slug.includes('openspace') ||
-                          post.category_slug.includes('super1'),
-                      }"
-                      :comment-count="post.comments"
-                      :is-premium="post.paywall === 'always'"
-                      :paywall="post.paywall"
-                      @comments="comments = !comments"
-                      @share="fbShare()"
-                    ></action-bar>
-                  </div>
-                </div>
-                <div class="full flex relative article-meta desktop-only-meta">
-                  <div class="meta-author-all">
-                    <app-link
-                      v-for="author in post.authors"
-                      :key="author.name"
-                      :to="author.url"
-                      class="meta-author flex"
-                      ><img
-                        v-if="author.image"
-                        :src="author.image"
-                        :alt="author.name"
-                      /><span>Piše</span
-                      ><span
-                        class="vcard author"
-                        rel="author"
-                        itemprop="author"
-                        >{{ author.name }}</span
-                      ></app-link
-                    >
-                  </div>
-                  <time class="meta-date" :datetime="post.time">{{
-                    post.time | parseTime
-                  }}</time>
-                  <span
-                    v-if="post.recommendations"
-                    class="meta-preporuke"
-                    itemprop="interactionStatistics"
-                    >{{ post.recommendations }} preporuka</span
-                  >
-
-                  <action-bar
-                    v-if="!post.audio"
-                    :class="{
-                      nonAudio: true,
-                      nonComments:
-                        post.category_slug.includes('superone') ||
-                        post.category_slug.includes('pitanje-zdravlja') ||
-                        post.category_slug.includes('openspace') ||
-                        post.category_slug.includes('super1'),
-                    }"
-                    :comment-count="post.comments"
-                    :is-premium="post.paywall === 'always'"
-                    :paywall="post.paywall"
-                    @comments="comments = !comments"
-                    @share="fbShare()"
-                  ></action-bar>
-                </div>
               </div>
               <div
                 v-if="post.type !== 'noimage' && (post.image.url || post.video)"
@@ -376,26 +242,71 @@
                 </template>
               </div>
               <!-- eslint-disable-next-line -->
-              <action-bar
-                v-if="post.audio"
-                :class="'with-audio'"
-                :comment-count="post.comments"
-                :audio="post.audio"
-                :is-premium="post.paywall === 'always'"
-                :paywall="post.paywall"
-                @play="() => $emit('play-audio')"
-                @gift="() => $emit('gift-article')"
-                @comments="comments = !comments"
-                @share="fbShare()"
-              ></action-bar>
               <p
                 v-if="post.perex"
                 class="perex"
                 itemprop="articleBody"
                 v-html="post.perex"
               ></p>
+              <div
+                v-if="post.type !== 'noimage'"
+                class="nothfive full flex relative article-meta"
+              >
+                <app-link
+                  v-for="author in post.authors"
+                  :key="author.name"
+                  :to="author.url"
+                  class="meta-author flex desktop-only"
+                  ><img
+                    v-if="author.image"
+                    :src="author.image"
+                    :alt="author.name"
+                  /><span>Piše</span
+                  ><span class="vcard author" rel="author" itemprop="author">{{
+                    author.name
+                  }}</span></app-link
+                >
+                <time class="meta-date" :datetime="post.time">{{
+                  post.time | parseTime
+                }}</time>
+                <span
+                  v-if="post.recommendations"
+                  class="meta-preporuke"
+                  itemprop="interactionStatistics"
+                  >{{ post.recommendations }} preporuka</span
+                >
+                <div class="sidebar-social flex">
+                  <client-only>
+                    <gift-article
+                      v-if="
+                        post.paywall === 'always' &&
+                        this.$store.state.user.token
+                      "
+                      :key="`gift-${post.id}`"
+                    ></gift-article>
+                  </client-only>
+                  <a href="#" @click.prevent="fbShare"
+                    ><font-awesome-icon
+                      :icon="['fab', 'facebook-f']"
+                      class="animate"
+                    ></font-awesome-icon>
+                  </a>
+                  <a
+                    :href="`https://twitter.com/intent/tweet?counturl=${encodeURI(
+                      post.social.path
+                    )}&text=${encodeURI(post.portal_title)}&url=${encodeURI(
+                      post.social.path
+                    )}&via=TelegramHR`"
+                    target="_blank"
+                    rel="nofollow"
+                    ><font-awesome-icon
+                      :icon="['fab', 'x-twitter']"
+                      class="animate"
+                    ></font-awesome-icon
+                  ></a>
+                </div>
+              </div>
             </div>
-
             <div class="full relative center single-top-banner">
               <div>
                 <ad-unit
@@ -668,9 +579,6 @@
   </div>
 </template>
 <style scoped>
-.meta-preporuke {
-  margin-left: 10px;
-}
 .commentsContainer {
   max-width: 710px;
   margin-left: auto;
@@ -678,57 +586,10 @@
   padding-left: 20px;
   padding-right: 20px;
 }
-.meta-all {
-  display: flex;
-  flex-direction: column;
-}
-.meta-author-all {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-}
-.meta-other {
-  display: flex;
-  justify-content: space-between;
-  width: 90vw;
-}
-.article-meta.desktop-only-meta {
-  display: none !important;
-}
-.commentary-meta-other {
-  display: flex;
-  align-items: center;
-  gap: 16px;
-  margin: 0 auto;
-  width: fit-content;
-}
-.commentary-meta-other .meta-date,
-.commentary-meta-other .meta-preporuke {
-  padding-top: 0 !important;
-  margin: unset !important;
-}
-.article-head-newsletter {
-  margin-bottom: 12px;
-}
-@media screen and (min-width: 600px) {
-  .article-meta.desktop-only-meta {
-    display: flex !important;
-    padding-right: 0 !important;
-  }
-  .article-meta.desktop-only-meta .meta-date {
-    margin-left: 10px;
-  }
-  .meta-all {
-    display: none;
-  }
-}
 @media screen and (min-width: 768px) {
   .commentsContainer {
     padding-left: 0px;
     padding-right: 0px;
-  }
-  .article-head-newsletter {
-    margin-bottom: 0px;
   }
 }
 </style>
@@ -774,21 +635,15 @@ export default {
       if (
         process.server &&
         this.$route.params.category !== 'preview' &&
-        post.social &&
-        post.social.path
+        post.social.path.replace('https://www.telegram.hr', '') !==
+          this.$route.path
       ) {
-        // Extract the path from social.path (remove any domain)
-        const socialPath = post.social.path
-          .replace('https://www.telegram.hr', '')
-          .replace('http://localhost:3000', '')
-          .replace('https://telegram-wp.ddev.site', '')
-
-        // Only redirect if paths don't match
-        if (socialPath !== this.$route.path) {
-          this.$telegram.context.res.statusCode = 301
-          this.$telegram.context.res.setHeader('Location', socialPath)
-          return
-        }
+        this.$telegram.context.res.statusCode = 301
+        this.$telegram.context.res.setHeader(
+          'Location',
+          post.social.path.replace('https://www.telegram.hr', '')
+        )
+        return
       }
       this.post = post
     } else {
@@ -1088,10 +943,6 @@ export default {
       if (this.$route.params.category === 'l') {
         window.history.replaceState({}, null, this.post.permalink)
       }
-      // Fetch user gifts if user is logged in and article has paywall
-      if (this.$store.state.user.token && this.post.paywall === 'always') {
-        this.$store.dispatch('gifts/getUserGifts')
-      }
     })
     this.widgetVariant = this.getWidgetVariant()
   },
@@ -1167,12 +1018,7 @@ export default {
           elementFn: () => {
             return document.getElementById('article-content')
           },
-          author_id:
-            this.post.authors && this.post.authors[0]
-              ? this.post.authors[0].display_name ||
-                this.post.authors[0].name ||
-                ''
-              : '',
+          author_id: this.post.authors[0].display_name,
         },
         tracker: {
           url: 'https://tracker.telegram.hr',
