b7b06f780e5b7dae15751dcf19e75f4373d186e9
Marinko Sevo <marinko.sevo@telegram.hr>
2026-01-15 10:08:31 +0100

    Add telesport 50-off page

---
+++ components/Elements/Pretplata/PretplataNewBoxTelesport.vue
@@ -0,0 +1,395 @@
+<template>
+  <div
+    :class="{ selected: selected }"
+    class="box-wrapper"
+    @click="handleSelect"
+  >
+    <div class="heading-section">
+      <div class="box-header">
+        <div class="title">
+          <h3 :class="{ 'premium-title': type === 'premium' }">
+            {{ card.title }}
+          </h3>
+          <p>
+            <span class="old-price">{{ card.oldPrice }}</span> {{ card.price
+            }}<span>/godišnje</span>
+          </p>
+        </div>
+        <div class="tag-wrapper">
+          <span
+            v-if="type === 'premium' || type === 'telesport-premium'"
+            class="benefit premium-recommended"
+          >
+            {{ type === 'premium' ? 'Preporučeno' : 'NAJBOLJA VRIJEDNOST' }}
+          </span>
+          <span
+            v-if="type !== 'telesport-premium'"
+            class="benefit"
+            :class="{ premium: type === 'premium' }"
+          >
+            {{ card.tag }}
+          </span>
+        </div>
+        <p v-if="type === 'telesport-premium'" class="tag-subtitle">
+          (Za samo <b>0,57 € tjedno</b> više dobivate puni pristup cijelom
+          Telegramu i čitanje bez reklama)
+        </p>
+      </div>
+      <div class="content">
+        <div
+          v-for="(feat, index) in card.features"
+          :key="index"
+          class="feature"
+        >
+          <font-awesome-icon class="benefit-icon" :icon="['fas', 'check']" />
+          <p class="feature-content" v-html="feat"></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      <button :class="{ selected: selected }">
+        {{ selected ? 'Odabrano' : card.buttonText }}
+      </button>
+      <span>{{ card.footerText }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PretplataNewBoxTelesport',
+  props: {
+    type: {
+      type: String,
+      default: 'standard',
+      validator: (value) =>
+        ['standard', 'premium', 'telesport', 'telesport-premium'].includes(
+          value
+        ),
+    },
+    subscriptionType: {
+      type: String,
+      default: 'individual',
+      validator: (value) => ['individual', 'family'].includes(value),
+    },
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    card() {
+      if (this.type === 'premium') {
+        return {
+          title: 'Premium',
+          price: '49€',
+          oldPrice: '99€',
+          tag: 'BEZ REKLAMA',
+          features: [
+            'Neograničeno čitanje Telegrama i Telesporta uz pristup arhivi',
+            'Ekskluzivni newsletteri s posebnim analizama nagrađivanih autora',
+            'Fokus na sadržaj - <b> čitanje bez reklama</b>',
+            '10 poklon članaka mjesečno',
+            'Posebni popusti i pogodnost Telegram Kluba',
+          ],
+          buttonText: 'Odaberite',
+          footerText: 'Otkažite u bilo kojem trenutku.',
+        }
+      }
+      if (this.type === 'telesport') {
+        return {
+          title: 'Standard',
+          price: '19,49€',
+          oldPrice: '39€',
+          tag: 'MANJE REKLAMA',
+          features: [
+            'Neograničeno čitanje Telesporta i pristup arhivi svih članaka',
+            'Ekskluzivni newsletteri s posebnim analizama nagrađivanih autora',
+            '10 poklon članaka mjesečno',
+            'Posebni popusti i pogodnost Telegram Kluba',
+          ],
+          buttonText: 'Iskoristi 50% popusta',
+          footerText:
+            'Nakon isteka prve godine pretplata se automatski obnavlja po punoj cijeni',
+        }
+      }
+      if (this.type === 'telesport-premium') {
+        return {
+          title: 'Telegram Premium',
+          price: '49€',
+          oldPrice: '99€',
+          tag: 'Najbolja vrijednost',
+          features: [
+            'Neograničeno čitanje Telegrama i Telesporta uz pristup arhivi',
+            'Ekskluzivni newsletteri s posebnim analizama nagrađivanih autora',
+            'Fokus na sadržaj - <b> čitanje bez reklama</b>',
+            '10 poklon članaka mjesečno',
+            'Posebni popusti i pogodnost Telegram Kluba',
+          ],
+          buttonText: 'Odaberite',
+          footerText:
+            'Nakon isteka prve godine pretplata se automatski obnavlja po punoj cijeni',
+        }
+      }
+      return {
+        title: 'Standard',
+        price: '39€',
+        oldPrice: '79€',
+        tag: 'MANJE REKLAMA',
+        features: [
+          'Neograničeno čitanje svih članaka Telegrama uz pristup arhivi',
+          'Ekskluzivni newsletteri s posebnim analizama nagrađivanih autora',
+          '10 poklon članaka mjesečno',
+          'Posebni popusti i pogodnosti Telegram Kluba',
+        ],
+        buttonText: 'Odaberite',
+        footerText: 'Otkažite u bilo kojem trenutku.',
+      }
+    },
+  },
+  methods: {
+    handleSelect() {
+      this.$emit('select', this.type)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.box-wrapper {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 402px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  background-color: white;
+  padding: 24px 25px 28px 25px;
+  border-radius: 8px;
+  gap: 24px;
+  justify-content: space-between;
+  flex: 1 1 0;
+}
+.box-wrapper.selected {
+  outline: 2px solid #9f9f9f;
+  outline-offset: -2px;
+}
+.heading-section {
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  text-align: center;
+  margin: 0 auto;
+}
+
+.heading-section .title {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+.box-header {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+.heading-section .title h3 {
+  font-family: 'Lora', sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+  color: black;
+}
+
+.premium-title {
+  background: linear-gradient(90.58deg, #946d29 15.27%, #f2c591 100.28%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.heading-section .title p {
+  font-family: 'Lora', sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 18px;
+  color: black;
+}
+
+.title p .old-price {
+  color: #9f9f9f;
+  text-decoration: line-through;
+}
+
+.tag-wrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  justify-content: center;
+}
+
+.tag-subtitle {
+  font-family: 'Barlow', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  text-align: center;
+  color: #000000;
+}
+.tag-subtitle b {
+  font-weight: 600;
+}
+
+.benefit {
+  font-family: 'Barlow', sans-serif;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+  color: #747474;
+  padding: 8px 9px;
+  border: 2px solid #747474;
+  border-radius: 8px;
+  width: fit-content;
+  letter-spacing: 1px;
+  background-color: #ffffff;
+}
+.benefit.premium {
+  padding: 8px 10px;
+  line-height: 14px;
+}
+.premium-recommended {
+  position: relative;
+  line-height: 1.6;
+  text-transform: uppercase;
+  background: linear-gradient(90.58deg, #946d29 15.27%, #f2c591 100.28%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  border-radius: 8px;
+  border: none;
+  display: inline-block;
+}
+
+.premium-recommended::before {
+  content: '';
+  position: absolute;
+  inset: 0;
+  border-radius: 8px;
+  padding: 2px;
+  background: linear-gradient(90.58deg, #946d29 15.27%, #f2c591 100.28%);
+  -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+  -webkit-mask-composite: xor;
+  mask-composite: exclude;
+  pointer-events: none;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding-top: 14px;
+  border-top: 1px solid #dfdfdf;
+}
+
+.content .feature {
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+}
+.feature-content {
+  font-family: 'Barlow', sans-serif;
+  font-size: 18px;
+  line-height: 24px;
+  color: black;
+  text-align: left;
+  align-items: center;
+}
+.feature-content ::v-deep(span) {
+  font-weight: 600;
+}
+.benefit-icon {
+  color: #8d5b31;
+}
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.footer button {
+  width: 100%;
+  padding: 16px;
+  background-color: #b5b5b5;
+  font-family: 'Barlow', sans-serif;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 18px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.footer button:hover {
+  background-color: #37ae37;
+  color: white;
+}
+.footer button:active {
+  background-color: #237023;
+}
+
+.footer button.selected {
+  background-color: #37ae37;
+  color: white;
+}
+
+.footer span {
+  font-family: 'Barlow', sans-serif;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 16px;
+  text-align: center;
+}
+
+@media screen and (min-width: 1024px) {
+  .box-wrapper {
+    padding: 28px 32px;
+    gap: 28px;
+    border-radius: 10px;
+  }
+  .heading-section .title h3 {
+    font-size: 28px;
+    line-height: 40px;
+  }
+  .heading-section {
+    gap: 16px;
+  }
+  .content {
+    padding-top: 16px;
+  }
+  .heading-section .title p {
+    font-size: 20px;
+    line-height: 20px;
+  }
+  .heading-section .title {
+    gap: 18px;
+  }
+  content {
+    padding-top: 16px;
+  }
+  .content .feature {
+    font-size: 16px;
+    line-height: 22px;
+  }
+  .tag-wrapper {
+    gap: 12px;
+  }
+  .benefit {
+    padding: 8px 10px;
+  }
+  .footer span {
+    line-height: 16px;
+  }
+}
+</style>
---
+++ pages/pretplata/telesport-50-off.vue
@@ -0,0 +1,402 @@
+<template>
+  <div>
+    <div v-if="loading" class="telegram-overlay">
+      <span class="telegram-loader"></span>
+    </div>
+    <div class="hero-telesport">
+      <div class="hero-wrapper">
+        <img
+          src="@/assets/img/telesport_logo_white.svg"
+          alt="Telesport logo"
+          class="hero-logo"
+        />
+        <p class="hero-text">
+          Rukometno prvenstvo traži vrhunske analize. Pratite ga na Telesportu
+          uz čak <span class="underline">50% popusta!</span>
+        </p>
+      </div>
+    </div>
+    <div class="content">
+      <div class="box-wrapper">
+        <div id="paymentBoxes" class="boxes">
+          <PretplataNewBoxTelesport
+            type="telesport"
+            :subscription-type="subscriptionType"
+            :selected="selectedPlan === 'standard'"
+            @select="selectPlan"
+          ></PretplataNewBoxTelesport>
+          <PretplataNewBoxTelesport
+            type="telesport-premium"
+            :subscription-type="subscriptionType"
+            :selected="selectedPlan === 'premium'"
+            @select="selectPlan"
+          ></PretplataNewBoxTelesport>
+        </div>
+      </div>
+      <div>
+        <PretplataPayment
+          :allow-terms-change="false"
+          :term="selectedTerm"
+          :payment-type="payment"
+          :annual-price="annualPrice"
+          :monthly-price="monthlyPrice"
+          :subscription-type="subscriptionType"
+          :gift="false"
+          @selectTerm="selectTerm"
+          @selectPaymentType="selectPaymentType"
+        />
+        <PretplataLogin
+          :email="email"
+          :can-log-in="canLogIn"
+          :login-error="loginError"
+          :login-url="loginUrl"
+          @updateCanLogIn="updateCanLogIn"
+          @updateEmail="updateEmail"
+        ></PretplataLogin>
+        <PretplataPaymentConfirm
+          :url-key="urlKey"
+          :loading="loading"
+          :can-log-in="canLogIn"
+          :logged-in="loggedIn"
+          :payment-type="payment"
+          :pack="pack"
+          :price="price"
+          :email="email"
+          :discounted-amount="discount"
+          copy-version="christmas"
+          @updateLoading="handleUpdateLoading"
+          @updateDiscount="handleUpdateDiscount"
+        ></PretplataPaymentConfirm>
+      </div>
+    </div>
+    <Features></Features>
+    <FAQ></FAQ>
+    <Testimonials></Testimonials>
+    <PretplataCTA
+      :text="'Blagdanska akcija traje do kraja ovog mjeseca'"
+      :link="{
+        url: '#paymentBoxes',
+        text: 'Iskoristite ponudu',
+      }"
+    ></PretplataCTA>
+    <client-only>
+      <!-- Chatbot Component -->
+      <Chatbot />
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      subscriptionType: 'individual',
+      loginUrl: '50-popust',
+      selectedPlan: 'premium',
+      selectedTerm: 'annual',
+      payment: 'trustpay_recurrent',
+      price: '',
+      pack: 'pretplata-standard',
+      term: 'pretplata-godisnje',
+      annualPrice: '',
+      monthlyPrice: '',
+      promo_code: '',
+      email: this.$store.state.user.email,
+      password: '',
+      showPassword: false,
+      terms: false,
+      privacy: false,
+      auth: 0,
+      urlKey: 'half-off-2025',
+      instance: null,
+      customerId: null,
+      iframeUrl: '',
+      canLogIn: true,
+      voucher_log_id: null,
+      discount: 0,
+      loadingPromo: false,
+      promo_error: '',
+    }
+  },
+  computed: {
+    loginError() {
+      return this.$store.state.user.error
+    },
+    loggedIn() {
+      return !!this.$store.state.user.id
+    },
+    userEmail() {
+      return this.$store.state.user.email
+    },
+  },
+  mounted() {
+    this.calculatePrice()
+  },
+  methods: {
+    handleSubscriptionChange(type) {
+      this.subscriptionType = type
+      this.calculatePrice()
+    },
+    handleUpdateLoading(state) {
+      this.loading = state
+    },
+    handleUpdateDiscount(value) {
+      this.discount = value
+    },
+    selectPlan(planType) {
+      if (planType === 'telesport') {
+        this.selectedPlan = 'standard'
+      } else if (planType === 'telesport-premium') {
+        this.selectedPlan = 'premium'
+      } else {
+        this.selectedPlan = planType
+      }
+      this.calculatePrice()
+    },
+    selectTerm(term) {
+      this.selectedTerm = term
+      this.calculatePrice()
+    },
+    selectPaymentType(paymentType) {
+      this.payment = paymentType
+      this.calculatePrice()
+    },
+    updateEmail(email) {
+      this.email = email
+    },
+    updateCanLogIn(value) {
+      this.canLogIn = value
+    },
+    calculatePrice() {
+      switch (this.subscriptionType) {
+        case 'individual':
+          this.discount = 0
+          this.urlKey = 'half-off-2025'
+          switch (this.selectedPlan) {
+            case 'standard':
+              this.pack =
+                'Telesport_Godišnja_Pretplata_50%_popust_za prvu godinu'
+              this.price = '19.49'
+              this.monthlyPrice = '7.99'
+              this.annualPrice = '19.49'
+              break
+            case 'premium':
+              this.pack =
+                'Telegram_Premium_Godišnja_Pretplata_50%_popust_za prvu godinu'
+              this.price = '49'
+              this.monthlyPrice = '9.99'
+              this.annualPrice = '49'
+              break
+          }
+          break
+        case 'family':
+          this.discount = 0
+          this.urlKey = 'family'
+          switch (this.selectedPlan) {
+            case 'standard':
+              if (this.selectedTerm === 'monthly') {
+                this.pack = 'telegram_standard_4_tjedna_family_pretplata_admin'
+                this.price = '16.79'
+              } else {
+                this.pack = 'telegram_standard_godisnja_family_pretplata_admin'
+                this.price = '166'
+              }
+              this.monthlyPrice = '16.79'
+              this.annualPrice = '166'
+              break
+            case 'premium':
+              if (this.selectedTerm === 'monthly') {
+                this.pack = 'telegram_premium_4_tjedna_family_pretplata_admin'
+                this.price = '20.99'
+              } else {
+                this.pack = 'telegram_premium_godisnja_family_pretplata_admin'
+                this.price = '208'
+              }
+              this.monthlyPrice = '20.99'
+              this.annualPrice = '208'
+              break
+          }
+          break
+      }
+    },
+  },
+
+  head() {
+    return {
+      title: 'Telesport Pretplata - 50% popusta za prvu godinu',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Odaberite jedan od paketa i podržite nas',
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: 'Odaberite jedan od paketa i podržite nas',
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          property: 'og:title',
+          content: 'Telesport Pretplata - 50% popusta za prvu godinu',
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          property: 'og:image',
+          content:
+            'https://www.telegram.hr/wp-content/uploads/2025/12/gift.png',
+        },
+        {
+          hid: 'og:url',
+          name: 'og:url',
+          property: 'og:url',
+          content: 'https://www.telegram.hr/pretplata/50-popust/',
+        },
+      ],
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: 'https://www.telegram.hr/pretplata/50-popust/',
+        },
+      ],
+      script: [
+        {
+          hid: 'jquery',
+          src: 'https://code.jquery.com/jquery-3.7.1.min.js',
+        },
+        {
+          hid: 'trustpay-popup',
+          src: 'https://mapi.trustpay.eu/mapi5/Scripts/TrustPay/popup.js',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.hero-telesport {
+  width: 100%;
+  min-height: 400px;
+  background: url('@/assets/img/pretplata/telesport/bg_telesport_50off_mob.png')
+    no-repeat center;
+  background-size: cover;
+}
+.hero-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 18px;
+  padding: 32px 24px;
+  max-width: 660px;
+  margin: 0 auto;
+}
+.hero-logo {
+  width: 100%;
+  max-width: 80px;
+}
+.hero-text {
+  font-family: 'Lora', sans-serif;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 28px;
+  text-align: center;
+  color: white;
+}
+.hero-text .underline {
+  text-decoration: underline;
+}
+@media screen and (min-width: 768px) {
+  .hero-telesport {
+    min-height: auto;
+    background: url('@/assets/img/pretplata/telesport/bg_telesport_50off.png')
+      no-repeat center;
+    background-size: cover;
+  }
+  .hero-wrapper {
+    gap: 32px;
+    padding: 48px 24px;
+  }
+  .hero-logo {
+    max-width: 100px;
+  }
+  .hero-text {
+    font-size: 28px;
+    line-height: 32px;
+  }
+}
+.telegram-overlay {
+  position: fixed;
+  top: 0;
+  z-index: 101;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  align-content: center;
+  background: rgb(0, 0, 0, 0.5);
+}
+.telegram-loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  border: 10px solid;
+  box-sizing: border-box;
+  animation: animloader 1s linear infinite alternate;
+}
+
+@keyframes animloader {
+  0% {
+    border-color: #ae3737 rgba(255, 255, 255, 0) rgba(255, 255, 255, 0)
+      rgba(255, 255, 255, 0);
+  }
+  33% {
+    border-color: #ae3737 #ae3737 rgba(255, 255, 255, 0) rgba(255, 255, 255, 0);
+  }
+  66% {
+    border-color: #ae3737 #ae3737 #ae3737 rgba(255, 255, 255, 0);
+  }
+  100% {
+    border-color: #ae3737 #ae3737 #ae3737 #ae3737;
+  }
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 24px 16px 60px 16px;
+  margin: 0 auto;
+  max-width: 865px;
+}
+.box-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  justify-content: space-between;
+}
+.boxes {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 100%;
+  margin: 0 auto;
+  max-width: 865px;
+}
+@media screen and (min-width: 1024px) {
+  .content {
+    padding: 40px 0px 60px 0px;
+  }
+  .boxes {
+    flex-direction: row;
+    gap: 60px;
+  }
+  .box-wrapper {
+    gap: 24px;
+  }
+}
+</style>
