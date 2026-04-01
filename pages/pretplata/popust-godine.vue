f9f192bc4099a147568e3cadff3a2e474fc22343
Marinko Sevo <marinko.sevo@telegram.hr>
2026-04-01 12:45:30 +0200

    Change share image

--- pages/pretplata/popust-godine.vue
+++ pages/pretplata/popust-godine.vue
@@ -245,7 +245,7 @@ export default {
           name: 'og:image',
           property: 'og:image',
           content:
-            'https://www.telegram.hr/wp-content/uploads/2025/12/gift.png',
+            'https://www.telegram.hr/wp-content/uploads/2021/01/tg-background.jpg',
         },
         {
           hid: 'og:url',
