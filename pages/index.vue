4680ccea2696d7221dd9506317fe5f2d25daaeb5
Marinko Sevo <marinko.sevo@telegram.hr>
2026-05-05 10:41:13 +0200

    Fix condition

--- pages/index.vue
+++ pages/index.vue
@@ -319,7 +319,7 @@
         style="width: 100%"
       />
     </app-link>
-    <Books v-show="!$store.state.user.access?.length" />
+    <Books v-show="$store.state.user.access?.length" />
     <client-only>
       <div v-if="!hasPremium" class="full relative">
         <offers-premium></offers-premium>
