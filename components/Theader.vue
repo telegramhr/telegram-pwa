45fc58fb4aaf5284e957db32f05b2568b0f5ea2d
Marinko Sevo <marinko.sevo@telegram.hr>
2026-07-24 16:53:09 +0200

    change utm to utm_content

--- components/Theader.vue
+++ components/Theader.vue
@@ -27,7 +27,7 @@
               :to="
                 $route.fullPath.includes('telesport')
                   ? '/pretplata/telesport'
-                  : '/pretplata/podrska-telegramu/?utm=theader'
+                  : '/pretplata/podrska-telegramu/?utm_content=theader'
               "
               class="signup-btn sub-btn"
               >Pretplatite se</app-link
@@ -402,7 +402,7 @@
               :to="
                 $route.fullPath.includes('telesport')
                   ? '/pretplata/telesport/'
-                  : '/pretplata/podrska-telegramu/?utm=theader'
+                  : '/pretplata/podrska-telegramu/?utm_content=theader'
               "
               :class="[
                 'newbtn',
