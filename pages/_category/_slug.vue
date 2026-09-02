559d7cf188faf864927bdf3b8d7cf08aa6b8d4eb
Marinko Sevo <marinko.sevo@telegram.hr>
2026-09-02 11:41:15 +0200

    Reenable plodine

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -584,7 +584,6 @@
               </transition>
               <div class="remp-banner"></div>
               <client-only>
-                <!-- on break till 1.9.
                 <portal
                   v-if="
                     useSparPortal &&
@@ -600,7 +599,7 @@
                   <div class="full">
                     <offers-premium></offers-premium>
                   </div>
-                </portal>-->
+                </portal>
                 <portal v-if="showQuiz" selector="#quiz-container">
                   <quiz
                     v-if="post.quiz"
