a785063aa1aadecdf9581feeb6cb870a8a6e20aa
Marinko Sevo <marinko.sevo@telegram.hr>
2026-09-03 17:16:40 +0200

    Add AI intext banner

---
+++ components/AiSummary/AiSummaryBannerIntext.vue
@@ -0,0 +1,20 @@
+<template>
+  <app-link :to="'https://www.samsung.com/hr/'">
+    <img
+      class="ai-banner desktop-only"
+      src="@/assets/img/ai-summary/samsung-desktop.png"
+    />
+    <img
+      class="ai-banner mobile-only"
+      src="@/assets/img/ai-summary/samsung-mobile.png"
+    />
+  </app-link>
+</template>
+
+<script>
+export default {
+  name: 'AiSummaryBanner',
+}
+</script>
+
+<style scoped></style>
